f77d111aab3e4dbe7ce9ff967bce724b964ba4cd
Adrien DELLE CAVE (Decryptus) <decryptus@proformatique.com>
2006-07-17 06:58:46 +0000

    Modification de la librairie gestion des erreurs ajout d'un append
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@3 dc85baef-7018-0410-bfbd-8a639da648bb

--- web-interface/www/index.php
+++ web-interface/www/index.php
@@ -1,15 +1,22 @@
 <?php
 
 if(($path_conf = realpath('../conf/')) === false)
-	die('ERR : CONF DIRECTORY NOT FOUND');
+	die('ERR : XIVO CONF DIRECTORY NOT FOUND');
 
-$conff_xivo = $path_conf.'/prepend.inc';
+$prependf_xivo = $path_conf.'/prepend.inc';
 
-if(!(is_file($conff_xivo) && is_readable($conff_xivo)))
-	die('ERR : CONF LOAD FILE XIVO NOT FOUND OR UNREADABLE');
+if(!(is_file($prependf_xivo) && is_readable($prependf_xivo)))
+	die('ERR : XIVO PREPEND FILE NOT FOUND OR UNREADABLE');
 
-require_once($conff_xivo);
+require_once($prependf_xivo);
 
-phpinfo();
+//phpinfo();
+
+$appendf_xivo = XIVO_PATH_CONF.'/append.inc';
+
+if(!(is_file($appendf_xivo) && is_readable($appendf_xivo)))
+	trigger_error('XIVO APPEND FILE NOT FOUND OR UNREADABLE',E_USER_ERROR);
+
+require_once($appendf_xivo);
 
 ?>
