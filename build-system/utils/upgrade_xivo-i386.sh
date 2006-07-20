6b5bb503e66524c27c87b9d43f35b635f471ccf4
Richard Braun <rbraun@proformatique.com>
2006-07-20 14:36:36 +0000

    Ajout du script de mise à jour pour le firmware glibc. Amélioration du support des drivers zaptel. Amélioration de la construction d'iptables.
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@8 dc85baef-7018-0410-bfbd-8a639da648bb

---
+++ build-system/utils/upgrade_xivo-i386.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# Xivo 0.1 - Upgrade Xivo.
+# Copyright (C) 2006 Richard Braun <rbraun@proformatique.com>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+set -e
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+sysrootfile=sysroot.img
+sysroot=https://192.168.0.220/cross/i486-linux-gnu/$sysrootfile
+
+root=$(ls -l /dev/root | cut -d '>' -f 2)
+
+# Trim spaces.
+root=$(echo $root)
+
+echo "upgrading firmware, please wait..."
+
+if [ "$root" = "/dev/hdc2" ]; then
+  newroot="/dev/hdc3"
+elif [ "$root" = "/dev/hdc3" ]; then
+  newroot="/dev/hdc2"
+else
+  echo "root partition is different from what was expected, aborting."
+  exit 1
+fi
+
+wget --no-check-certificate $sysroot -O $newroot > /dev/null
+mkdir /tmp/hdc1
+mount -o sync /dev/hdc1 /tmp/hdc1
+sed -ie "s%$root%$newroot%" /tmp/hdc1/boot/grub/menu.lst
+umount /tmp/hdc1
+
+echo "firmware upgraded, rebooting..."
+reboot
