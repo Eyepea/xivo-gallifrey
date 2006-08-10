ff8c1d4bd8f6ebf361131c3d215b7f719b5791a7
Richard Braun <rbraun@proformatique.com>
2006-08-10 15:02:32 +0000

    Implémentation du support Realtime Static.
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@76 dc85baef-7018-0410-bfbd-8a639da648bb

--- res_sqlite/res_sqlite.c
+++ res_sqlite/res_sqlite.c
@@ -23,12 +23,57 @@
 #include <sqlite.h>
 #include <pthread.h>
 #include <asterisk/cdr.h>
+#include <asterisk/lock.h>
 #include <asterisk/config.h>
 #include <asterisk/logger.h>
 #include <asterisk/module.h>
 #include <asterisk/options.h>
 
-#include "res_sqlite.h"
+#define RES_SQLITE_NAME "res_sqlite"
+#define RES_SQLITE_DRIVER "sqlite"
+#define RES_SQLITE_DESCRIPTION "Resource Module for SQLite 2"
+#define RES_SQLITE_CONF_FILE "res_sqlite.conf"
+
+#define SET_VAR(config, to, from) \
+{ \
+  int __error; \
+  __error = set_var(&to, #to, from->value); \
+  if (__error) \
+    { \
+      ast_config_destroy(config); \
+      unload_config(); \
+      return 1; \
+    } \
+}
+
+#define CHECK_PARAMETER(name) \
+{ \
+  if (name == NULL) \
+    { \
+      ast_log(LOG_ERROR, "Undefined parameter %s\n", #name); \
+      return 1; \
+    } \
+}
+
+#define RES_SQLITE_BEGIN \
+{ \
+  int __i; \
+  for (__i = 0; __i < 10; __i++) \
+    {
+
+#define RES_SQLITE_END(error) \
+      if (error != SQLITE_BUSY && error != SQLITE_LOCKED) \
+        break; \
+      usleep(1000); \
+    } \
+}
+
+struct cfg_entry_args
+{
+  struct ast_config *cfg;
+  struct ast_category *cat;
+  char *cat_name;
+};
 
 static int cdr_registered;
 static sqlite *db;
@@ -41,6 +86,17 @@ static int load_config(void);
 static void unload_config(void);
 static int check_vars(void);
 static int cdr_handler(struct ast_cdr *cdr);
+static struct ast_config * config_handler(const char *database,
+                                          const char *table, const char *file,
+                                          struct ast_config *cfg);
+
+AST_MUTEX_DEFINE_STATIC(mutex);
+
+static struct ast_config_engine sqlite_engine =
+{
+  .name = RES_SQLITE_DRIVER,
+  .load_func = config_handler
+};
 
 /*
  * Taken from Asterisk 1.2 cdr_sqlite.so.
@@ -109,6 +165,12 @@ static char *sql_add_cdr_entry =
 "	'%q',"
 "	'%q'"
 ");";
+
+static char *sql_get_config_table =
+"SELECT *"
+"	FROM '%q'"
+"	WHERE filename = '%q' AND commented = 0"
+"	ORDER BY category;";
 
 static int
 set_var(char **var, char *name, char *value)
@@ -191,32 +253,111 @@ static int
 cdr_handler(struct ast_cdr *cdr)
 {
   char *errormsg;
-  int i, error;
+  int error;
 
-  for (i = 0; i < 3; i++)
-    {
-      error = sqlite_exec_printf(db, sql_add_cdr_entry, NULL, NULL, &errormsg,
-                                 cdr_table, cdr->clid, cdr->src, cdr->dst,
-                                 cdr->dcontext, cdr->channel, cdr->dstchannel,
-                                 cdr->lastapp, cdr->lastdata, cdr->start.tv_sec,
-                                 cdr->answer.tv_sec, cdr->end.tv_sec,
-                                 cdr->duration, cdr->billsec, cdr->disposition,
-                                 cdr->amaflags, cdr->accountcode, cdr->uniqueid,
-                                 cdr->userfield);
+  ast_mutex_lock(&mutex);
 
-      if (!error || (error && (error != SQLITE_BUSY && error != SQLITE_LOCKED)))
-        break;
+  RES_SQLITE_BEGIN
+    error = sqlite_exec_printf(db, sql_add_cdr_entry, NULL, NULL, &errormsg,
+                               cdr_table, cdr->clid, cdr->src, cdr->dst,
+                               cdr->dcontext, cdr->channel, cdr->dstchannel,
+                               cdr->lastapp, cdr->lastdata, cdr->start.tv_sec,
+                               cdr->answer.tv_sec, cdr->end.tv_sec,
+                               cdr->duration, cdr->billsec, cdr->disposition,
+                               cdr->amaflags, cdr->accountcode, cdr->uniqueid,
+                               cdr->userfield);
+  RES_SQLITE_END(error)
 
-      usleep(200);
-    }
+  ast_mutex_unlock(&mutex);
 
   if (error)
     {
       ast_log(LOG_ERROR, RES_SQLITE_NAME ": %s\n", errormsg);
       free(errormsg);
+      return 1;
     }
 
   return 0;
+}
+
+/*
+ * Columns are :
+ *  0 id
+ *  1 filename
+ *  2 category
+ *  3 commented
+ *  4 var_name
+ *  5 var_val
+ */
+static int
+add_cfg_entry(void *arg, int argc, char **argv, char **columnNames)
+{
+  struct cfg_entry_args *args;
+  struct ast_variable *var;
+
+  if (argc != 6)
+    {
+      ast_log(LOG_ERROR, RES_SQLITE_NAME ": Corrupt table\n");
+      return 1;
+    }
+
+  args = (struct cfg_entry_args *)arg;
+
+  if (args->cat_name == NULL || strcmp(args->cat_name, argv[2]))
+    {
+      args->cat = ast_category_new(argv[2]);
+
+      if (args->cat == NULL)
+        {
+          ast_log(LOG_ERROR, RES_SQLITE_NAME ": Unable to allocate category\n");
+          return 1;
+        }
+
+      free(args->cat_name);
+      args->cat_name = strdup(argv[2]);
+
+      if (args->cat_name == NULL)
+        {
+          ast_log(LOG_ERROR, RES_SQLITE_NAME ": Unable to allocate category "
+                  "nam\n");
+          return 1;
+        }
+
+      ast_category_append(args->cfg, args->cat);
+    }
+
+  var = ast_variable_new(argv[4], argv[5]);
+  ast_variable_append(args->cat, var);
+  return 0;
+}
+
+static struct ast_config *
+config_handler(const char *database, const char *table, const char *file,
+               struct ast_config *cfg)
+{
+  struct cfg_entry_args args;
+  char *errormsg;
+  int error;
+
+  args.cfg = cfg;
+  args.cat = NULL;
+  args.cat_name = NULL;
+
+  RES_SQLITE_BEGIN
+    error = sqlite_exec_printf(db, sql_get_config_table, add_cfg_entry,
+                               &args, &errormsg, config_table, file);
+  RES_SQLITE_END(error)
+
+  free(args.cat_name);
+
+  if (error)
+    {
+      ast_log(LOG_ERROR, RES_SQLITE_NAME ": %s\n", errormsg);
+      free(errormsg);
+      return NULL;
+    }
+
+  return cfg;
 }
 
 int
@@ -244,8 +385,10 @@ load_module(void)
       return 1;
     }
 
-  error = sqlite_exec_printf(db, "SELECT COUNT(AcctId) FROM %s;", NULL, NULL,
-                             &errormsg, cdr_table);
+  RES_SQLITE_BEGIN
+    error = sqlite_exec_printf(db, "SELECT COUNT(AcctId) FROM %s;", NULL, NULL,
+                               &errormsg, cdr_table);
+  RES_SQLITE_END(error)
 
   if (error)
     {
@@ -260,8 +403,10 @@ load_module(void)
           return 1;
         }
 
-      error = sqlite_exec_printf(db, sql_create_cdr_table, NULL, NULL,
-              &errormsg, cdr_table);
+      RES_SQLITE_BEGIN
+        error = sqlite_exec_printf(db, sql_create_cdr_table, NULL, NULL,
+                &errormsg, cdr_table);
+      RES_SQLITE_END(error)
 
       if (error)
         {
@@ -272,6 +417,7 @@ load_module(void)
         }
     }
 
+  ast_config_engine_register(&sqlite_engine);
   error = ast_cdr_register(RES_SQLITE_NAME, RES_SQLITE_DESCRIPTION,
                            cdr_handler);
 
@@ -290,6 +436,8 @@ unload_module(void)
 {
   if (cdr_registered)
     ast_cdr_unregister(RES_SQLITE_NAME);
+
+  ast_config_engine_deregister(&sqlite_engine);
 
   if (db != NULL)
     sqlite_close(db);
