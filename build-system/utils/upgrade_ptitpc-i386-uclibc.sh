bc13fb12f7f555094604ce15c5a71cc6fd5cbd72
Richard Braun <rbraun@proformatique.com>
2006-10-16 17:28:18 +0000

    Interception d'appel, file d'attente, modification de la mise à jour du firmware.
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@152 dc85baef-7018-0410-bfbd-8a639da648bb

--- build-system/utils/upgrade_ptitpc-i386-uclibc.sh
+++ build-system/utils/upgrade_ptitpc-i386-uclibc.sh
@@ -1,35 +1,16 @@
 #!/bin/sh
 
-# Xivo 0.3 - Upgrade ptitpc-i386-uclibc.
-# Copyright (C) 2006 Richard Braun <rbraun@proformatique.com>
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
 set -e
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 sysrootfile=sysroot.img
-sysroot=http://192.168.0.220/cross/dists/ptitpc-i386-uclibc/$sysrootfile
+sysroot=https://192.168.0.220/cross/dists/ptitpc-i386-uclibc/$sysrootfile
 
 root=$(ls -l /dev/root | cut -d '>' -f 2)
 
 # Trim spaces.
 root=$(echo $root)
-
-echo "upgrading firmware, please wait..."
 
 if [ "$root" = "hda2" ]; then
   newroot="/dev/hda3"
@@ -45,6 +26,3 @@ mount /boot
 root="/dev/$root"
 sed -ie "s%$root%$newroot%" /boot/boot/grub/menu.lst
 umount /boot
-
-echo "firmware upgraded, rebooting..."
-reboot
