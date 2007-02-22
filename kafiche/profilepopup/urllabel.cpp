e756c52801ee79d8d15be4a7c9070577f7b0bf83
Thomas Bernard <tbernard@proformatique.com>
2007-02-22 17:35:54 +0000

    fixed some stuff to have clickable links
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@184 dc85baef-7018-0410-bfbd-8a639da648bb

--- kafiche/profilepopup/urllabel.cpp
+++ kafiche/profilepopup/urllabel.cpp
@@ -1,16 +1,21 @@
 #include <QLabel>
 #include "urllabel.h"
 
+/*! \brief Constructor
+ *
+ * Call QLabel constructor, set properties to
+ * allow link to be clickable and put some html magic
+ * into the label text. */
 UrlLabel::UrlLabel(const QString & url, QWidget * parent)
 : QLabel(parent)
 {
 	QString displaytext, text;
 	//setForegroundRole( QPalette::Link );
-	//setOpenExternalLinks(true);	// new in Qt 4.2
+	setOpenExternalLinks(true);	// new in Qt 4.2
 	setTextInteractionFlags( Qt::LinksAccessibleByMouse
-                            | Qt::LinksAccessibleByKeyboard
-	                        | Qt::TextSelectableByMouse
-	                        | Qt::TextSelectableByKeyboard );
+                            | Qt::LinksAccessibleByKeyboard );
+	//                        | Qt::TextSelectableByMouse
+	//                        | Qt::TextSelectableByKeyboard );
 	if(url.startsWith("mailto:"))
 	{
 		displaytext = url.mid(7);
