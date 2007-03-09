f9589d4a0d8d17a2b038c551a2e594bcfb9a32f3
Thomas Bernard <tbernard@proformatique.com>
2007-03-09 18:15:34 +0000

    set timer to 1sec
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@218 dc85baef-7018-0410-bfbd-8a639da648bb

--- switchboard/client/main.cpp
+++ switchboard/client/main.cpp
@@ -10,7 +10,8 @@ int main(int argc, char * * argv)
 	MainWindow mainwin(&engine);
 	mainwin.show();
 	engine.setAddress("192.168.0.159", 8080);
-	engine.startTimer(10000);		//connectSocket();
+	//engine.startTimer(10000);		//connectSocket();
+	engine.startTimer(1000);		//connectSocket();
 	return app.exec();
 }
 
