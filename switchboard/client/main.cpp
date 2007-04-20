a5918458128e284dabe016b67db0a5543e3f5c49
Thomas Bernard <tbernard@proformatique.com>
2007-04-20 09:08:15 +0000

    Mise en place des traductions
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@371 dc85baef-7018-0410-bfbd-8a639da648bb

--- switchboard/client/main.cpp
+++ switchboard/client/main.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <QTranslator>
+#include <QLocale>
 #include "switchboardwindow.h"
 #include "switchboardengine.h"
 #include "mainwindow.h"
@@ -9,11 +11,18 @@ int main(int argc, char * * argv)
 	QCoreApplication::setOrganizationDomain("proformatique.com");
 	QCoreApplication::setApplicationName("SwitchBoard");
 	QApplication app(argc, argv);
+
+	QTranslator qtTranslator;
+	qtTranslator.load("qt_" + QLocale::system().name());
+	app.installTranslator(&qtTranslator);
+
+	QTranslator switchboardTranslator;
+	switchboardTranslator.load("switchboard_" + QLocale::system().name());
+	app.installTranslator(&switchboardTranslator);
+
 	SwitchBoardEngine engine;
 	MainWindow mainwin(&engine);
 	mainwin.show();
-	//engine.setAddress("192.168.0.77", 5081); //connectSocket();
-	//engine.start();
 	engine.startTimer(1000);
 	return app.exec();
 }
--- switchboard/client/switchboard_fr.ts
+++ switchboard/client/switchboard_fr.ts
@@ -3,57 +3,105 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="23"/>
+        <location filename="mainwindow.cpp" line="122"/>
         <source>&amp;File</source>
         <translation>&amp;Fichier</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="25"/>
+        <location filename="mainwindow.cpp" line="124"/>
         <source>S&amp;tart</source>
         <translation>&amp;Démarrer</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="29"/>
+        <location filename="mainwindow.cpp" line="129"/>
         <source>Sto&amp;p</source>
         <translation>&amp;Arrêter</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="33"/>
+        <location filename="mainwindow.cpp" line="135"/>
         <source>&amp;Configure</source>
         <translation>&amp;Configuration</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="37"/>
+        <location filename="mainwindow.cpp" line="140"/>
         <source>&amp;Quit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="41"/>
+        <location filename="mainwindow.cpp" line="144"/>
         <source>&amp;Help</source>
         <translation>&amp;Aide</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="43"/>
+        <location filename="mainwindow.cpp" line="147"/>
         <source>About &amp;Qt</source>
         <translation>A propos de &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="125"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="130"/>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="136"/>
+        <source>Open the configuration dialog</source>
+        <translation>Ouvre la fenêtre de configuration</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="146"/>
+        <source>&amp;About</source>
+        <translation>&amp;A propos</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="198"/>
+        <source>About XIVO SwitchBoard</source>
+        <translation>à propos du SwitchBoard XIVO</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="200"/>
+        <source>&lt;h3&gt;About&lt;/h3&gt;&lt;p&gt;To be continued&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchPanel</name>
+    <message>
+        <location filename="searchpanel.cpp" line="15"/>
+        <source>Name or number to search</source>
+        <translation>Nom ou numéro à rechercher</translation>
     </message>
 </context>
 <context>
     <name>SwitchBoardConfDialog</name>
     <message>
-        <location filename="switchboardconf.cpp" line="14"/>
-        <source>Configuration</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="switchboardconf.cpp" line="19"/>
+        <source>Configuration</source>
+        <translation>Configuration</translation>
+    </message>
+    <message>
+        <location filename="switchboardconf.cpp" line="24"/>
         <source>Server host :</source>
+        <translation>Adresse IP serveur :</translation>
+    </message>
+    <message>
+        <location filename="switchboardconf.cpp" line="28"/>
+        <source>Server port :</source>
+        <translation>Port du serveur :</translation>
+    </message>
+    <message>
+        <location filename="switchboardconf.cpp" line="34"/>
+        <source>Width :</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="switchboardconf.cpp" line="23"/>
-        <source>Server port :</source>
-        <translation type="unfinished"></translation>
+        <location filename="switchboardconf.cpp" line="41"/>
+        <source>Autoconnect at startup</source>
+        <translation>Se connecter au démarrage</translation>
     </message>
 </context>
 </TS>
