3a34b0daf90462312fab7df270d6ae9ace96b3b9
Guillaume Knispel <gknispel@proformatique.com>
2007-05-07 15:14:18 +0000

    Makes Phones/__init__.py works even if the calling script is not in the parent directory of this package
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@484 dc85baef-7018-0410-bfbd-8a639da648bb

--- provisioning/Phones/__init__.py
+++ provisioning/Phones/__init__.py
@@ -1,16 +1,14 @@
 # -*- coding: iso-8859-15 -*-
 
-import re, sys, os, os.path
+import re, os, os.path
 
 # WARNING: non order preserving
 def uniqlist(lst):
 	if not lst: return []
 	return (dict(map(lambda a: (a,1), lst))).keys()
 
-# WARNING: this function only makes sens for internal packages shipped
-# with a calling script living in a parent directory...
 def get_this_internal_package_path():
-	return os.path.join(os.path.abspath(sys.path[0]), __name__)
+	return os.path.dirname(os.path.abspath(__file__))
 
 # Python doesn't really want us to do that because of
 # compatibility with stupid operating systems, but thanks
