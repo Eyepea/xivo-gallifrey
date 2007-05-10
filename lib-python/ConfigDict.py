141c5188904b406dc7ebf9e4e0258d91a2ea713b
Guillaume Knispel <gknispel@proformatique.com>
2007-05-10 12:23:41 +0000

    Added more powerfull functions in ConfigDict, to be able to load from multiple sections and to specify types without the need of default values
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@520 dc85baef-7018-0410-bfbd-8a639da648bb

--- lib-python/ConfigDict.py
+++ lib-python/ConfigDict.py
@@ -7,7 +7,7 @@ Copyright (C) 2007, Proformatique
 
 REV_DATE = "$Revision$ $Date$"
 
-from ConfigParser import ConfigParser, NoSectionError
+from ConfigParser import ConfigParser, NoSectionError, NoOptionError
 
 def FillDictFromConfigSection(dicttofill, confpars_obj, section):
 	"""Fills dicttofill with the content of the wanted section of the
@@ -29,8 +29,7 @@ def FillDictFromConfigSection(dicttofill, confpars_obj, section):
 		dico = FillDictFromConfigSection({}, conf, sec)
 	
 	"""
-	items = confpars_obj.items(section)
-	for name,val in items:
+	for name,val in confpars_obj.items(section):
 		if name in dicttofill:
 			t = type(dicttofill[name])
 			if t is not type(None):
@@ -38,8 +37,64 @@ def FillDictFromConfigSection(dicttofill, confpars_obj, section):
 				try:
 					val = t(val)
 				except ValueError:
-					raise ValueError, "Content of \"%s\" in configuration file should be of type \"%s\", but the actual value found there \"%s\" is not" % (name, str(t), oldval)
+					raise ValueError, "Content of \"%s\" in section \"%s\" of configuration file should be of type \"%s\", but the actual value found there \"%s\" is not" % (name, section, str(t), oldval)
 		dicttofill[name] = val
 	return dicttofill
 	
-__all__ = ["NoSectionError", "FillDictFromConfigSection"]
+def FillDictFromMultipleConfig(dicttofill, confpars_obj, sect_mapping):
+	"""Acts the same as FillDictFromConfigSection(), but instead of filling
+	the dictionary with every entries of a given section of the
+	configuration file, uses the other dictionary sect_mapping to get the
+	list of keys to populate in dicttofill and their corresponding section
+	and name in the configuration file.
+	
+	Also the wanted type is not deduced from an existing entry in
+	dicttofill but is instead explicitely given in a sect_mapping entry.
+	
+	The structure of sect_mapping is the following:
+	
+		{ 'key_for_dicttofill': ('section', 'name', type), ... }
+	
+	An entry missing in the configuration file will result in the raise of
+	the corresponding ConfigParser exception.
+	
+	"""
+	for key,(section, name, t) in sect_mapping.iteritems():
+		val = confpars_obj.get(section, name)
+		if t is not None and t is not type(None):
+			oldval = val
+			try:
+				val = t(val)
+			except ValueError:
+				raise ValueError, "Content of \"%s\" in section \"%s\" of configuration file should be of type \"%s\", but the actual value found there \"%s\" is not" % (name, section, str(t), oldval)
+		dicttofill[key] = val
+	return dicttofill
+
+def FillDictFromMultipleConfigOpt(dicttofill, confpars_obj, sect_mapping):
+	"""Acts the same as FillDictFromMultipleConfig(), but won't propagate
+	exception in case an entry is missing - that is
+	ConfigParser.NoSectionError and ConfigParser.NoOptionError are catched.
+	
+	When a configuration file entry is missing, the corresponding key is not
+	inserted in dicttofill. Nevertheless if the corresponding key
+	pre-existed in dicttofill, it is not removed.
+	
+	"""
+	for key,(section, name, t) in sect_mapping.iteritems():
+		try:
+			val = confpars_obj.get(section, name)
+			if t is not None and t is not type(None):
+				oldval = val
+				try:
+					val = t(val)
+				except ValueError:
+					raise ValueError, "Content of \"%s\" in section \"%s\" of configuration file should be of type \"%s\", but the actual value found there \"%s\" is not" % (name, section, str(t), oldval)
+			dicttofill[key] = val
+		except NoSectionError, NoOptionError:
+			pass
+	return dicttofill
+
+__all__ = ["NoSectionError", "NoOptionError",
+           "FillDictFromConfigSection",
+           "FillDictFromMultipleConfig",
+	   "FillDictFromMultipleConfigOpt"]
