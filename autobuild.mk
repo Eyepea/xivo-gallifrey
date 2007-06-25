2bdd66ae95492cf5ff9e281e5b40d08970123f0e
Marc Dequènes (Duck) <mdequenes@proformatique.com>
2007-06-25 13:33:00 +0000

    added FORCE_UNSCRAMBLED support
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@967 dc85baef-7018-0410-bfbd-8a639da648bb

--- autobuild.mk
+++ autobuild.mk
@@ -4,6 +4,9 @@ WRK_DIR ?= $(shell pwd)
 DEB_DESTDIR ?= $(WRK_DIR)/..
 DEB_TAR_EXTRA_OPTIONS ?=
 TARBALL_DIR = .
+DEB_TB_DEPS ?=
+DEB_TB_SCRAMBLED ?= "0"
+FORCE_UNSCRAMBLED ?=
 
 # internal variables
 MAINREV=$(shell cat $(WRK_DIR)/../VERSION)
@@ -13,6 +16,9 @@ XIVO_REV=$(MAINREV)~svn$(SVNREV)
 
 tarball-dep:
 	@echo "$(DEB_TB_DEPS)"
+
+tarball-scrambled:
+	@echo "$(DEB_TB_SCRAMBLED)"
 
 tarball: prepare-tarball do-tarball clean-tarball
 
