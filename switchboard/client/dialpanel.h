c112c4a622cba368607b547e1e9d736458397354
Corentin Le Gall <clegall@proformatique.com>
2007-07-06 15:18:15 +0000

    unnecessary includes removed in order for this class to be reused for the xivoclient
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@1072 dc85baef-7018-0410-bfbd-8a639da648bb

--- switchboard/client/dialpanel.h
+++ switchboard/client/dialpanel.h
@@ -21,7 +21,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #define __DIALPANEL_H__
 #include <QWidget>
 #include <QList>
-#include "peeritem.h"
 
 class QVBoxLayout;
 class QLineEdit;
