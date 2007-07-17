a42e8c50dd6e50e260aa4d190debad7cc6a4f075
Guillaume Knispel <gknispel@proformatique.com>
2007-07-17 08:50:00 +0000

    ThreadingHTTPServer put appart
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@1155 dc85baef-7018-0410-bfbd-8a639da648bb

---
+++ lib-python/ThreadingHTTPServer.py
@@ -0,0 +1,30 @@
+"""Threaded HTTP Server
+
+Copyright (C) 2007, Proformatique
+
+"""
+
+__version__ = "$Revision$ $Date$"
+__license__ = """
+    Copyright (c) 2001, 2002, 2003, 2004 Python Software Foundation;
+    Copyright (C) 2007, Proformatique
+                                        All Rights Reserved
+
+    Under PSF LICENSE AGREEMENT FOR PYTHON
+    See the following URI for the full license:
+        http://www.python.org/download/releases/2.4.4/license/
+"""
+
+class ThreadingHTTPServer(SocketServer.ThreadingTCPServer):
+	"""Same as HTTPServer, but derivates from ThreadingTCPServer instead
+	of TCPServer so that any instance of ProvHttpHandler created for any
+	incoming connection runs in its own thread.
+	
+	"""
+	allow_reuse_address = 1    # Seems to make sense in testing environment
+	def server_bind(self):
+		"""Override server_bind to store the server name."""
+		SocketServer.TCPServer.server_bind(self)
+		host, port = self.socket.getsockname()[:2]
+		self.server_name = socket.getfqdn(host)
+		self.server_port = port
