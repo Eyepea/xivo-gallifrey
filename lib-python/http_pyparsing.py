ddf5579b7f8aa0dda84172980a14cfeece24944b
Guillaume Knispel <gknispel@proformatique.com>
2007-07-19 18:53:36 +0000

    HTTP Related Parsing using pyparsing
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@1189 dc85baef-7018-0410-bfbd-8a639da648bb

---
+++ lib-python/http_pyparsing.py
@@ -0,0 +1,48 @@
+"""HTTP Related Parsing using pyparsing
+
+Copyright (C) 2007, Proformatique
+
+"""
+
+__version__ = "$Revision$ $Date$"
+__license__ = """
+    Copyright (C) 2007, Proformatique
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+from pyparsing import Word, Suppress, ZeroOrMore, QuotedString, Group, LineEnd
+
+__BYTES_VAL = ''.join(map(chr,xrange(0,256)))
+
+HTTP_CHAR = ''.join(map(chr,xrange(128)))
+HTTP_SEPARATORS = '()<>@,;:\\/[]?={} \t"'
+HTTP_CTLS = ''.join(map(chr,xrange(32)))+chr(127)
+HTTP_TOKCHAR = HTTP_CHAR.translate(__BYTES_VAL, HTTP_SEPARATORS + HTTP_CTLS)
+
+HTTP_TOKEN = Word( HTTP_TOKCHAR )
+HTTP_QUOTED = QuotedString( '"', escChar = '\\', multiline = True )
+HTTP_VALUE = HTTP_TOKEN | HTTP_QUOTED
+HTTP_PARAMETER = HTTP_TOKEN + Suppress( '=' ) + HTTP_VALUE
+
+HTTP_TRANSFER_EXTENSION = HTTP_TOKEN + Group( ZeroOrMore( Suppress( ';' ) + Group( HTTP_PARAMETER ) ) )
+HTTP_TRANSFER_CODING = Group( HTTP_TRANSFER_EXTENSION ) + ZeroOrMore( Suppress( ',' ) + Group( HTTP_TRANSFER_EXTENSION ) )
+
+HTTP_TRANSFER_CODING_LINE = HTTP_TRANSFER_CODING + LineEnd()
+
+__all__ = [ 'HTTP_CHAR', 'HTTP_SEPARATORS', 'HTTP_CTLS', 'HTTP_TOKCHAR',
+            'HTTP_TOKEN', 'HTTP_QUOTED', 'HTTP_VALUE', 'HTTP_PARAMETER',
+	    'HTTP_TRANSFER_EXTENSION', 'HTTP_TRANSFER_CODING',
+	    'HTTP_TRANSFER_CODING_LINE' ]
