1eb99eff3cdce5bafe75971f495d0ea427052379
Marc Dequènes (Duck) <mdequenes@proformatique.com>
2007-08-03 16:43:55 +0000

    Tarball is stored in the filer. Now provides a script to fetch tarball on Digium's site, and repack it in a DFSG-compliant way
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@1295 dc85baef-7018-0410-bfbd-8a639da648bb

---
+++ asterisk/fetch_tarball.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+usage()
+{
+	echo "Usage: $0 <version> <destination>"
+}
+
+UPVERSION=$1
+if [ -z "${UPVERSION}" ]; then
+	usage
+	exit 1
+fi
+DEST_PATH=$2
+if [ -z "${DEST_PATH}" ]; then
+	usage
+	exit 1
+fi
+if [ ! -d "${DEST_PATH}" ]; then
+	"Destination does not exists or is not a directory"
+	exit 1
+fi
+
+FILENAME="asterisk_${UPVERSION}~dfsg.orig.tar.gz"
+
+if [ -e "${DEST_PATH}/${FILENAME}" ]; then
+	echo "A tarball already exist for this version ; remove it if you want to regenerate."
+	exit 0
+fi
+
+UPFILENAME="asterisk_${UPVERSION}.orig.tar.gz"
+URL="ftp://ftp2.digium.com/pub/asterisk/releases/asterisk-${UPVERSION}.tar.gz"
+
+echo "Downloading ${UPFILENAME} from ${URL}"
+wget -N -nv -T10 -t3 -O ${DEST_PATH}/${UPFILENAME} ${URL}
+if [ $? != 0 ]; then
+	rm -f ${DEST_PATH}/${UPFILENAME}
+	echo "Could not find tarball."
+	exit 2
+fi
+
+echo "Repacking as DFSG-free..."
+mkdir -p ${DEST_PATH}/asterisk-${UPVERSION}.tmp/
+cd ${DEST_PATH}/asterisk-${UPVERSION}.tmp
+tar xfz ../${UPFILENAME}
+if [ -e "asterisk-${UPVERSION}" ]; then
+	find . -depth -type f -name 'fpm-*.mp3' -exec rm -rf {} \;
+	rm -rf asterisk-${UPVERSION}/codecs/ilbc asterisk-${UPVERSION}/contrib/firmware/
+	tar cfz ../${FILENAME} asterisk-${UPVERSION}
+else
+	echo "Source tarball layout changed. Check by yourself in '${DEST_PATH}/asterisk-${UPVERSION}.tmp/'."
+	exit 2
+fi
+
+echo "Cleaning up..."
+cd - >/dev/null
+rm -rf ${DEST_PATH}/${UPFILENAME} ${DEST_PATH}/asterisk-${UPVERSION}.tmp/
+
