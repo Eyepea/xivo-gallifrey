1306c286f98e0fb42a2067c47b797733fc2e5740
Guillaume Knispel <gknispel@proformatique.com>
2007-11-02 01:58:53 +0000

    [behavior] Mainly: the .query() method of an anysql cursor now escapes column names, which is needed for Xivo. Also added inline documentation (unfinished).
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@1733 dc85baef-7018-0410-bfbd-8a639da648bb

--- lib-python/xivo/anysql.py
+++ lib-python/xivo/anysql.py
@@ -39,7 +39,9 @@ METHOD_C14N_URI = 2
 METHOD_ESCAPE = 3
 
 class cursor(object):
+	"XXX"
 	class row(list):
+		"XXX"
 		def __init__(self, col2idx_map, dbapi2_result):
 			list.__init__(self, dbapi2_result)
 			self.__col2idx_map = col2idx_map
@@ -50,38 +52,41 @@ class cursor(object):
 				return list.__getitem__(self, self.__col2idx_map[k])
 
 	def __init__(self, dbapi2_cursor, methods):
+		"XXX"
 		self.__dbapi2_cursor = dbapi2_cursor
 		self.__methods = methods
 
 	def close(self):
+		"As in DBAPI2.0"
 		self.__dbapi2_cursor.close()
 
 	def __preparequery(self, sql_query, columns):
-		"""WARNING: Columns is not escaped
-		WARNING: You can't pass a ${columns} literal to the underlying .execute() method
+		"""WARNING: You can't both pass a ${columns} literal to the
+		underlying .execute() method and use the columns escaping,
+		injection and symbolic access mechanism.
 		WARNING: It is not recommended to SELECT *
 		"""
-
-		if "${columns}" not in sql_query and columns:
-			raise TypeError("received columns but ${columns} not in query")
-
 		if columns:
+			if "${columns}" not in sql_query:
+				raise TypeError("received columns but ${columns} not in query")
+
+			escape = self.__methods[METHOD_ESCAPE]
+
 			self.__col2idx_map = {}
 			col_list = []
 
 			for idx,col in enumerate(columns):
 				self.__col2idx_map[col] = idx
-				col_list.append(col)
+				col_list.append(escape(col))
 
-		if "${columns}" in sql_query:
-			escape = self.__methods[METHOD_ESCAPE]
-			return sql_query.replace("${columns}", ",".join([escape(col) )
-		elif columns:
-			raise TypeError("received columns but ${columns} not in query")
+			return sql_query.replace("${columns}", ",".join(col_list))
 		else:
+			self.__col2idx_map = None
+		
 			return sql_query
 
 	def query(self, sql_query, columns = None, parameters = None):
+		"XXX"
 		tmp_query = self.__preparequery(sql_query, columns)
 
 		if parameters is None:
@@ -90,13 +95,24 @@ class cursor(object):
 			self.__dbapi2_cursor.execute(tmp_query, parameters)
 
 	def querymany(self, sql_query, columns, seq_of_parameters):
+		"XXX"
 		tmp_query = self.__preparequery(sql_query, columns)
 		self.__dbapi2_cursor.executemany(tmp_query, seq_of_parameters)
 
 	def fetchone(self):
+		"""As in DBAPI2.0 (except the fact rows are not tuples but
+		lists so if you try to modify them, you will succeed instead of
+		the correct behavior that would be that an exception would have
+		been raised)
+		"""
 		return self.row(self.__col2idx_map, self.__dbapi2_cursor.fetchone())
 
 	def fetchmany(self, size = None):
+		"""As in DBAPI2.0 (except the fact rows are not tuples but
+		lists so if you try to modify them, you will succeed instead of
+		the correct behavior that would be that an exception would have
+		been raised)
+		"""
 		if size is None:
 			manyrows = self.__dbapi2_cursor.fetchmany()
 		else:
@@ -105,12 +121,19 @@ class cursor(object):
 		return [self.row(self.__col2idx_map, dbapi2_row) for dbapi2_row in manyrows]
 
 	def fetchall(self):
+		"""As in DBAPI2.0 (except the fact rows are not tuples but
+		lists so if you try to modify them, you will succeed instead of
+		the correct behavior that would be that an exception would have
+		been raised)
+		"""
 		return [self.row(self.__col2idx_map, dbapi2_row) for dbapi2_row in self.__dbapi2_cursor.fetchall()]
 
 	def setinputsizes(self, sizes):
+		"As in DBAPI2.0"
 		self.__dbapi2_cursor.setinputsizes(sizes)
 
 	def setoutputsize(self, size, column = None):
+		"As in DBAPI2.0"
 		if column is None:
 			self.__dbapi2_cursor.setoutputsize(size)
 		else:
@@ -128,25 +151,84 @@ class cursor(object):
 	def __set_arraysize(self, arraysize):
 		self.__dbapi2_cursor.arraysize = arraysize
 
-	description = property(__get_description)
-	rowcount = property(__get_rowcount)
-	arraysize = property(__get_arraysize, __set_arraysize)
+	description = property(__get_description, None, None, "As in DBAPI2.0")
+	rowcount = property(__get_rowcount, None, None, "As in DBAPI2.0")
+	arraysize = property(__get_arraysize, __set_arraysize, None, "As in DBAPI2.0")
 
 class connection:
+	"""This class is a Anysql wrapper for DBAPI2.0 connection objects.
+	It does not do much: essentially it just pass method calls to the
+	underlying DBAPI2.0 object, with the exception of the .cursor()
+	method which will not directly returns the DBAPI2.0 cursor but wrap
+	it using the anysql.cursor class.
+	WARNING: instantiation of this class is private to this module and is
+	automatically performed by connect_by_uri()
+	WARNING: even if the instantiation mecanism is put appart, this class
+	still does not constitute a DBAPI2.0 Connection Object because the
+	.cursor() method generate a Cursor Object that is not a real DBAPI2.0
+	Cursor Object (it has a slightly different and incompatible method
+	.query() instead of .execute())
+	"""
 	def __init__(self, dbapi2_conn, methods):
+		"""Contructor: takes two arguments
+		  - dbapi2_conn: underlying DBAPI2.0 connection object
+		  - methods: private object describing the underlying backend,
+		    internally generated by this module using information
+		    provided by the backend at registration time.
+		WARNING: instantiation of this class is private to this module
+		and is automatically performed by connect_by_uri()
+		"""
 		self.__dbapi2_conn = dbapi2_conn
 		self.__methods = methods
 
 	def close(self):
+		"""As in DBAPI2.0:
+		Close the connection now (rather than whenever __del__ is
+		called).  The connection will be unusable from this point
+		forward; an Error (or subclass) exception will be raised
+		if any operation is attempted with the connection. The
+		same applies to all cursor objects trying to use the
+		connection.  Note that closing a connection without
+		committing the changes first will cause an implicit
+		rollback to be performed.
+		"""
 		self.__dbapi2_conn.close()
 
 	def commit(self):
+		"""As in DBAPI2.0:
+		Commit any pending transaction to the database. Note that
+		if the database supports an auto-commit feature, this must
+		be initially off. An interface method may be provided to
+		turn it back on.
+
+		Database modules that do not support transactions should
+		implement this method with void functionality.
+		"""
 		self.__dbapi2_conn.commit()
 
 	def rollback(self):
+		"""As in DBAPI2.0:
+		This method is optional since not all databases provide
+		transaction support. [3]
+
+		In case a database does provide transactions this method
+		causes the database to roll back to the start of any
+		pending transaction.  Closing a connection without
+		committing the changes first will cause an implicit
+		rollback to be performed.
+		"""
 		self.__dbapi2_conn.rollback()
 
 	def cursor(self):
+		"""Returns a new Cursor Object using the connection,
+		that is NOT a DBAPI2.0 cursor.
+		An underlying real DBAPI2.0 cursor will be asked to the
+		underlying backend for use by the cursor object returned by
+		this method, and the former will only be referenced by the
+		latter.
+		The Cursor Object returned by this method will be an instance
+		of the class cursor of this module.
+		"""
 		return cursor(self.__dbapi2_conn.cursor(), self.__methods)
 
 def __compare_api_level(als1, als2):
@@ -168,13 +250,17 @@ def register_uri_backend(uri_scheme, create_method, module, c14n_uri, escape):
 	
 	The associated module must be compliant with DBAPI v2.0 but will not
 	be directly used for other purposes than compatibility testing.
-
+	
 	c14n_uri must be a function that takes one string argument (the same
 	form that the one that would be passed to connect_by_uri) and returns
 	its canonicalized form in an implementation dependant way. This
 	includes transforming any local pathname into an absolute form.
 	c14n_uri can also be None, in which case the behavior will be the same
 	as the one of the identity function.
+	
+	escape must be a function that takes one string argument (an unescaped
+	column name) and returns an escaped version for use as an escaped
+	column name in an SQL query for this backend.
 	
 	If something obviously not compatible is tried to be registred,
 	NotImplementedError is raised. """
@@ -213,7 +299,7 @@ def connect_by_uri(sqluri):
 	A malformed URI will result in an exception being raised by the
 	supporting URI parsing module. """
 	methods = _get_methods_by_uri(sqluri)
-	uri_connect_method = methods[0]
+	uri_connect_method = methods[METHOD_CONNECT]
 	return connection(uri_connect_method(sqluri), methods)
 
 def c14n_uri(sqluri):
@@ -222,10 +308,11 @@ def c14n_uri(sqluri):
 	
 	If no backend is found for this uri method, a NotImplementedError
 	will be raised. """
-	uri_c14n_method = _get_methods_by_uri(sqluri)[2]
+	uri_c14n_method = _get_methods_by_uri(sqluri)[METHOD_C14N_URI]
 	if not uri_c14n_method:
 		return sqluri
 	return uri_c14n_method(sqluri)
 
-__all__ = ["register_uri", "connect_by_uri", "c14n_uri",
+__all__ = ["register_uri_backend", "connect_by_uri", "c14n_uri",
+           "cursor", "connection",
            "any_paramstyle", "any_threadsafety", "any_apilevel"]
