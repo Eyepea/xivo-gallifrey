4e634847716f427374e00a296f638d5d04f88439
Guillaume Knispel <gknispel@proformatique.com>
2008-01-02 10:22:41 +0000

    [refactoring] patches moved in patches/ and BUILD.sh easily configurable [feature] added 'make report' at the end of BUILD.sh
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@2229 dc85baef-7018-0410-bfbd-8a639da648bb

--- openssl-0.9.8g-mingw/BUILD.sh
+++ openssl-0.9.8g-mingw/BUILD.sh
@@ -2,14 +2,24 @@
 
 set -e
 
-tar xzvf openssl-0.9.8g.tar.gz 
-cd openssl-0.9.8g
-patch -p1 < ../build_under_broken_cygwin.patch
-patch -p1 < ../build_cygwin_gnu_make_3_81_cross_mingw.patch
-patch -p1 < ../build_broken_cross_mingw_out_implib.patch
-patch -p1 < ../build_no_at_in_Makefile.patch
-patch -p1 < ../build_shared_set_x.patch
-../deref_symlinks.sh
-PERL="c:/cygwin/bin/perl" ./Configure mingw -L/cygwin/home/Administrateur/zlib-1.2.3/ -I/cygwin/home/Administrateur/zlib-1.2.3/ no-idea no-mdc2 no-rc5 zlib enable-tlsext threads shared 
-../deref_symlinks.sh
-make --debug=j PERL="c:/cygwin/bin/perl"
+# CONFIGURATION
+VERSION=0.9.8g
+OPENSSL_DIR=openssl-$VERSION
+OPENSSL_TARBALL=openssl-$VERSION.tar.gz
+PERL_PATH="c:/cygwin/bin/perl"
+ZLIB_PATH="/cygwin/home/Administrateur/zlib-1.2.3/"
+OPENSSL_OPTIONS=no-idea no-mdc2 no-rc5 zlib enable-tlsext threads shared
+DEREF_SYMLINKS=deref_symlinks.sh
+
+# BUILD
+tar xzvf $OPENSSL_TARBALL
+cp -a patches $OPENSSL_DIR/patches
+cd $OPENSSL_DIR
+for each in `cat patches/series`; do
+	patch -p1 < patches/$each
+done
+../$DEREF_SYMLINKS
+PERL="$PERL_PATH" ./Configure mingw -L"$ZLIB_PATH" -I"$ZLIB_PATH" $OPENSSL_OPTIONS
+../$DEREF_SYMLINKS
+make --debug=j PERL="$PERL_PATH"
+make report PERL="$PERL_PATH"
