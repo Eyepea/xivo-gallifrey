360ddf5295681921be48cba7870f05989f78675b
Richard Braun <rbraun@proformatique.com>
2008-01-03 17:20:49 +0000

    [feature] Boss secretary filter partially implemented. [feature] Use the new agitb module for better debugging. [bugfix] Some formats were adapted.
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@2246 dc85baef-7018-0410-bfbd-8a639da648bb

--- lib-python/xivo/agi_helpers.py
+++ lib-python/xivo/agi_helpers.py
@@ -124,7 +124,7 @@ def set_fwd_vars(agi, cursor, type, typeval, appval, type_varname, typeval1_varn
 		agi.set_variable(typeval1_varname, typeval.replace(",", ";").replace("|", ";"))
 	elif type == 'user':
 		cursor.query("SELECT ${columns} FROM userfeatures "
-                             "WHERE id = %s "
+                             "WHERE id = %d "
                              "AND internal = 0 "
                              "AND commented = 0",
                              ('number', 'context'),
@@ -139,7 +139,7 @@ def set_fwd_vars(agi, cursor, type, typeval, appval, type_varname, typeval1_varn
 	elif type == 'group':
 		cursor.query("SELECT ${columns} FROM groupfeatures INNER JOIN queue "
                              "ON groupfeatures.name = queue.name "
-                             "WHERE groupfeatures.id = %s "
+                             "WHERE groupfeatures.id = %d "
                              "AND groupfeatures.deleted = 0 "
                              "AND queue.category = 'group' "
                              "AND queue.commented = 0",
@@ -155,7 +155,7 @@ def set_fwd_vars(agi, cursor, type, typeval, appval, type_varname, typeval1_varn
 	elif type == 'queue':
 		cursor.query("SELECT ${columns} FROM queuefeatures INNER JOIN queue "
                              "ON queuefeatures.name = queue.name "
-                             "WHERE queuefeatures.id = %s "
+                             "WHERE queuefeatures.id = %d "
                              "AND queue.category = 'queue' "
                              "AND queue.commented = 0",
                              [('queuefeatures.' + x) for x in ('number', 'context')],
@@ -170,16 +170,194 @@ def set_fwd_vars(agi, cursor, type, typeval, appval, type_varname, typeval1_varn
 	elif type == 'meetme':
 		cursor.query("SELECT ${columns} FROM meetmefeatures INNER JOIN meetme "
                              "ON meetmefeatures.meetmeid = meetme.id "
-                             "WHERE meetmefeatures.id = %s "
+                             "WHERE meetmefeatures.id = %d "
                              "AND meetme.commented = 0",
                              [('meetmefeatures.' + x) for x in ('number', 'context')],
                              (typeval,))
 		res = cursor.fetchone()
 
 		if not res:
-			dp_break(agi, "Database inconsistency: unable to find linked destination conference room'%s'" % typeval)
+			dp_break(agi, "Database inconsistency: unable to find linked destination conference room '%s'" % typeval)
 
 		agi.set_variable(typeval1_varname, res['meetmefeatures.number'])
 		agi.set_variable(typeval2_varname, res['meetmefeatures.context'])
 	else:
 		dp_break(agi, "Unknown destination type '%s'" % type)
+
+class bsf_member:
+	def __init__(self, type, userid, number, ringseconds):
+		self.type = type
+		self.userid = userid
+		self.number = number
+		self.interface = None
+
+		if ringseconds == 0:
+			self.ringseconds = ""
+
+		else:
+			self.ringseconds = ringseconds
+
+	def __str__(self):
+		return ("Call filter member object :\n"
+                       "Type:        %s\n"
+                       "User ID:     %d\n"
+                       "Number:      %s\n"
+                       "Interface:   %s\n"
+                       "RingSeconds: %s"
+                       % (self.type, self.userid, self.number, self.interface, self.ringseconds))
+
+	def agi_str(self, agi):
+		s = str(self)
+
+		for line in s.splitlines():
+			agi.verbose(line)
+
+class bsfilter:
+	def __init__(self, agi, cursor, boss_number, boss_context):
+		self.id = None
+		self.context = None
+		self.mode = None
+		self.zone = None
+		self.callerdisplay = None
+		self.ringseconds = None
+		self.boss = None
+		self.secretaries = None
+
+		cursor.query("SELECT ${columns} FROM callfilter "
+                             "INNER JOIN callfiltermember "
+                             "ON callfilter.id = callfiltermember.callfilterid "
+                             "INNER JOIN userfeatures "
+                             "ON callfiltermember.typeval = userfeatures.id "
+                             "WHERE callfilter.type = 'bosssecretary' "
+                             #"AND callfilter.active = 1 "
+                             "AND callfilter.commented = 0 "
+                             "AND callfiltermember.type = 'user' "
+                             "AND callfiltermember.bstype = 'boss' "
+                             #"AND callfiltermember.active = 1 "
+                             "AND userfeatures.number = %s "
+                             "AND userfeatures.context = %s "
+                             "AND userfeatures.internal = 0 "
+                             "AND userfeatures.bsfilter = 'boss' "
+                             "AND userfeatures.commented = 0",
+                             ('callfilter.id', 'callfilter.bosssecretary',
+                              'callfilter.zone', 'callfilter.callerdisplay',
+                              'callfilter.ringseconds', 'callfiltermember.ringseconds',
+                              'userfeatures.id', 'userfeatures.protocol',
+                              'userfeatures.protocolid', 'userfeatures.name'),
+                             (boss_number, boss_context))
+		res = cursor.fetchone()
+
+		if not res:
+			raise LookupError, ("Unable to find call filter ID for boss (number = '%s', context = '%s')" % (boss_number, boss_context))
+
+		protocol = res['userfeatures.protocol']
+		protocolid = res['userfeatures.protocolid']
+		name = res['userfeatures.name']
+
+		self.id = res['callfilter.id']
+		self.context = boss_context
+		self.mode = res['callfilter.bosssecretary']
+		self.zone = res['callfilter.zone']
+		self.callerdisplay = res['callfilter.callerdisplay']
+		self.ringseconds = res['callfilter.ringseconds']
+		self.boss = bsf_member('boss', res['userfeatures.id'], boss_number, res['callfiltermember.ringseconds'])
+		self.secretaries = []
+
+		if self.ringseconds == 0:
+			self.ringseconds = ""
+
+		if protocol in ("sip", "iax"):
+			interface = protocol.upper() + "/" + name
+		elif protocol == "custom":
+			cursor.query("SELECT ${columns} FROM usercustom "
+                                     "WHERE id = %d "
+                                     "AND commented = 0 "
+                                     "AND category = 'user'",
+                                     ('interface',),
+                                     (protocolid,))
+			res = cursor.fetchone()
+
+			if not res:
+				agi_helpers.dp_break(agi, "Database inconsistency: unable to find custom user (name = '%s', context = '%s')" % (name, context))
+
+			interface = res['interface']
+		else:
+			agi_helpers.dp_break(agi, "Unknown protocol '%s'" % protocol)
+
+		self.boss.interface = interface
+
+		cursor.query("SELECT ${columns} FROM callfiltermember INNER JOIN userfeatures "
+                             "ON callfiltermember.typeval = userfeatures.id "
+                             "WHERE callfiltermember.callfilterid = %d "
+                             "AND callfiltermember.type = 'user' "
+                             "AND callfiltermember.bstype = 'secretary' "
+                             #"AND callfiltermember.active = 1 "
+                             "AND userfeatures.context = %s "
+                             "AND userfeatures.internal = 0 "
+                             "AND userfeatures.bsfilter = 'secretary' "
+                             "AND userfeatures.commented = 0 "
+                             "ORDER BY priority ASC",
+                             ('userfeatures.id', 'userfeatures.protocol', 'userfeatures.protocolid',
+                              'userfeatures.name', 'userfeatures.number', 'userfeatures.ringseconds'),
+                             (self.id, boss_context))
+		res = cursor.fetchall()
+
+		if not res:
+			raise LookupError, ("Unable to find secretaries for call filter ID %d (context = '%s')" % (self.id, boss_context))
+
+		for row in res:
+			protocol = row['userfeatures.protocol']
+			protocolid = row['userfeatures.protocolid']
+			name = row['userfeatures.name']
+			secretary = bsf_member('secretary', row['userfeatures.id'],
+                                               row['userfeatures.number'], row['userfeatures.ringseconds'])
+
+			if protocol in ("sip", "iax"):
+				interface = protocol.upper() + "/" + name
+			elif protocol == "custom":
+				cursor.query("SELECT ${columns} FROM usercustom "
+                                             "WHERE id = %d "
+                                             "AND commented = 0 "
+                                             "AND category = 'user'",
+                                             ('interface',),
+                                             (protocolid,))
+				res = cursor.fetchone()
+
+				if not res:
+					agi_helpers.dp_break(agi, "Database inconsistency: unable to find custom user (name = '%s', context = '%s')" % (name, context))
+
+				interface = res['interface']
+			else:
+				agi_helpers.dp_break(agi, "Unknown protocol '%s'" % protocol)
+
+			secretary.interface = interface
+			self.secretaries.append(secretary)
+			self.agi_str(agi)
+
+	def __str__(self):
+		return ("Call filter object :\n"
+                       "Context:       %s\n"
+                       "Mode:          %s\n"
+                       "Zone:          %s\n"
+                       "CallerDisplay: %s\n"
+                       "RingSeconds:   %s\n"
+                       "Boss:\n%s\n"
+                       "Secretaries:\n%s"
+                       % (self.context, self.mode, self.zone, self.callerdisplay,
+                          self.ringseconds, self.boss, '\n'.join((str(secretary) for secretary in self.secretaries))))
+
+	def agi_str(self, agi):
+		s = str(self)
+
+		for line in s.splitlines():
+			agi.verbose(line)
+
+	def check_zone(self, zone):
+		if self.zone == "all":
+			return True
+		elif self.zone == "internal" and zone == "intern":
+			return True
+		elif self.zone == "external" and zone == "extern":
+			return True
+		else:
+			return False
