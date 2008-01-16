36a3dcf1bd4546ff523ea764b1f1289b3e0ee26c
Guillaume Knispel <gknispel@proformatique.com>
2008-01-16 21:47:36 +0000

    [build] slightly better BUILD.sh that should allow OpenSSL to be built on the new KVM hosted Win32 env, using the MinGW env blessed by Trolltech
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@2344 dc85baef-7018-0410-bfbd-8a639da648bb

--- openssl-0.9.8g-mingw/BUILD.sh
+++ openssl-0.9.8g-mingw/BUILD.sh
@@ -7,9 +7,18 @@ VERSION=0.9.8g
 OPENSSL_DIR=openssl-$VERSION
 OPENSSL_TARBALL=openssl-$VERSION.tar.gz
 PERL_PATH="c:/cygwin/bin/perl"
-ZLIB_PATH="/cygwin/home/Administrateur/zlib-1.2.3/"
+if [ ! -n "$ZLIB_PATH" ] ; then
+	ZLIB_PATH="/cygwin/home/winbuild/zlib-1.2.3"
+fi
 OPENSSL_OPTIONS="no-idea no-mdc2 no-rc5 zlib enable-tlsext threads shared"
 DEREF_SYMLINKS=deref_symlinks.sh
+
+if [ ! -e /cygdrive/c$ZLIB_PATH ] ; then
+	echo "/cygdrive/c$ZLIB_PATH not found"
+	echo "Please fix ZLIB_PATH in this script, or export it as an environment"
+	echo "var before invocation."
+	exit 1
+fi
 
 # BUILD
 if [ -e $OPENSSL_DIR ] ; then
