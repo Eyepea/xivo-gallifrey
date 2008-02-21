f2a3a78d766570b1566b709b7ab43531a1c25873
Guillaume Knispel <gknispel@proformatique.com>
2008-02-21 01:51:15 +0000

    [license] update copyright date
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@2454 dc85baef-7018-0410-bfbd-8a639da648bb

--- lib-python/xivo/urisup.py
+++ lib-python/xivo/urisup.py
@@ -6,7 +6,7 @@ Copyright (C) 2007, Proformatique
 
 __version__ = "$Revision$ $Date$"
 __license__ = """
-    Copyright (C) 2007, Proformatique
+    Copyright (C) 2007, 2008  Proformatique
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
