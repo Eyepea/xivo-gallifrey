f121cda4c28d6efddc86dc71e0effadbc0db63ff
Guillaume Knispel <gknispel@proformatique.com>
2008-03-07 16:49:40 +0000

    [license] trivial
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@2556 dc85baef-7018-0410-bfbd-8a639da648bb

--- ami_aoriginate/ami_aoriginate.c
+++ ami_aoriginate/ami_aoriginate.c
@@ -5,7 +5,7 @@
  * Copyright (C) 1999 - 2005, Digium, Inc.
  * Copyright (C) 2008  Proformatique
  *
- * This package is free software; you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 2 dated June, 1991.
  *
--- func_strsubst/func_strsubst.c
+++ func_strsubst/func_strsubst.c
@@ -2,7 +2,7 @@
  * String Substitution Function for Asterisk
  * Copyright (C) 2008  Proformatique
  *
- * This package is free software; you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 2 dated June, 1991.
  *
