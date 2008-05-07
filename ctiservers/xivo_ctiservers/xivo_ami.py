ed376b37981c8930314dc680da17bb9451f36fcb
Corentin Le Gall <clegall@proformatique.com>
2008-05-07 11:14:16 +0000

    back to r2839
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@2953 dc85baef-7018-0410-bfbd-8a639da648bb

--- ctiservers/xivo_ctiservers/xivo_ami.py
+++ ctiservers/xivo_ctiservers/xivo_ami.py
@@ -336,7 +336,6 @@ class AMIClass:
         # \brief Adds a Queue
         def queueadd(self, queuename, interface):
                 try:
-                        print 'AMI-QueueAdd %s %s' % (queuename, interface)
                         ret = self.sendcommand('QueueAdd', [('Queue', queuename),
                                                             ('Interface', interface),
                                                             ('Penalty', '1'),
@@ -351,7 +350,6 @@ class AMIClass:
         # \brief Removes a Queue
         def queueremove(self, queuename, interface):
                 try:
-                        print 'AMI-QueueRemove %s %s' % (queuename, interface)
                         ret = self.sendcommand('QueueRemove', [('Queue', queuename),
                                                                ('Interface', interface)])
                         reply = self.readresponse('')
@@ -364,7 +362,6 @@ class AMIClass:
         # \brief (Un)Pauses a Queue
         def queuepause(self, queuename, interface, paused):
                 try:
-                        print 'AMI-QueuePause %s %s %s' % (queuename, interface, paused)
                         ret = self.sendcommand('QueuePause', [('Queue', queuename),
                                                               ('Interface', interface),
                                                               ('Paused', paused)])
