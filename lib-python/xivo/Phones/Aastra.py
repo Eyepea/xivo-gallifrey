1ecae8b5e2e50431a53304c21ba0b939778961e3
Guillaume Knispel <gknispel@proformatique.com>
2008-05-22 15:59:53 +0000

    [license] corrected year
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@3063 dc85baef-7018-0410-bfbd-8a639da648bb

--- lib-python/xivo/Phones/Aastra.py
+++ lib-python/xivo/Phones/Aastra.py
@@ -8,7 +8,7 @@ Copyright (C) 2008  Proformatique
 
 __version__ = "$Revision$ $Date$"
 __license__ = """
-    Copyright (C) 2007, 2008  Proformatique
+    Copyright (C) 2008  Proformatique
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
