d842e66abd38e7c5c2b416d405cd0fad7f8d6a32
Guillaume Knispel <gknispel@proformatique.com>
2008-06-19 15:54:41 +0000

    test
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@3331 dc85baef-7018-0410-bfbd-8a639da648bb

---
+++ lib-python/Experimentations/tst_autoattrib_conf.py
@@ -0,0 +1,42 @@
+import yaml
+from xivo import xivo_config
+from xivo import trace_stderr
+
+conf = xivo_config.load_configuration("""
+resolvConf: {}
+ipConfs:
+    static_001:
+        address:     192.168.0.200
+        netmask:     255.255.255.0
+        broadcast:   192.168.0.255
+        gateway:     192.168.0.254
+    static_002:
+        address:     192.168.0.200
+        netmask:     255.255.255.0
+    static_003:
+        address:     192.168.1.200
+        netmask:     255.255.255.0
+vlans:
+    vs_0001:
+        0: void
+netIfaces:
+    eth0: void
+    eth1: void
+services:
+    voip:
+        ipConf: static_001
+        addresses:
+            voipServer: 192.168.0.200
+            bootServer: 192.168.0.200
+            voipRange:
+            - 192.168.0.100
+            - 192.168.0.199
+""", trace_stderr)
+
+#import tracer
+#import time
+#tracer.enable_tofp(open("traces.%s" % time.time(), 'w'))
+
+print xivo_config.autoattrib_conf(conf)
+print
+print yaml.dump(conf, default_flow_style=False, indent=4)
