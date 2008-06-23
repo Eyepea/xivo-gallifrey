b4342cb49f03b50381546aa3658ffbaef2a6b9f7
Richard Braun <rbraun@proformatique.com>
2008-06-23 16:37:41 +0000

    [interface] New internal call dial plan.
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@3366 dc85baef-7018-0410-bfbd-8a639da648bb

--- agid/xivo_agid/modules/incoming_user_set_features.py
+++ agid/xivo_agid/modules/incoming_user_set_features.py
@@ -23,14 +23,23 @@ from xivo_agid import agid
 from xivo_agid import objects
 
 def incoming_user_set_features(agi, cursor, args):
-	srcnum = agi.get_variable('REAL_SRCNUM')
-	dstnum = agi.get_variable('REAL_DSTNUM')
-	context = agi.get_variable('REAL_CONTEXT')
-	zone = agi.get_variable('REAL_CALLTYPE')
+	srcnum = agi.get_variable('XIVO_SRCNUM')
+	dstnum = agi.get_variable('XIVO_DSTNUM')
+	dstid = agi.get_variable('XIVO_DSTID')
+	context = agi.get_variable('XIVO_CONTEXT')
+	zone = agi.get_variable('XIVO_CALLORIGIN')
 	bypass_filter = agi.get_variable('XIVO_CALLFILTER_BYPASS')
 
 	feature_list = objects.FeatureList(agi, cursor)
-	user = objects.User(agi, cursor, feature_list, number = dstnum, context = context)
+
+	if dstnum and context:
+		user = objects.User(agi, cursor, feature_list, number = dstnum, context = context)
+		agi.set_variable('XIVO_DSTID', user.id)
+	elif dstid:
+		user = objects.User(agi, cursor, feature_list, xid = dstid)
+	else:
+		agi.dp_break("No dstnum@context or userid given, unable to lookup user")
+
 	filter = user.filter
 
 	# Special case. If a boss-secretary filter is set, the code will prematurely
