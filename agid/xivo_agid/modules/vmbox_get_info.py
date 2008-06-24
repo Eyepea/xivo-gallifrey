ebcae5530201850c683b62cc994795876de7b8a0
Richard Braun <rbraun@proformatique.com>
2008-06-24 13:37:14 +0000

    [feature] Dialplan code and agid module to directly check a voicemail box.
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@3376 dc85baef-7018-0410-bfbd-8a639da648bb

---
+++ agid/xivo_agid/modules/vmbox_get_info.py
@@ -0,0 +1,34 @@
+__version__ = "$Revision$ $Date$"
+__license__ = """
+    Copyright (C) 2006, 2007, 2008  Proformatique
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA..
+"""
+
+from xivo_agid import agid
+from xivo_agid import objects
+
+def vmbox_get_info(agi, cursor, args):
+	vmboxid = int(agi.get_variable('XIVO_VMBOXID'))
+
+	vmbox = objects.VMBox(agi, cursor, vmboxid)
+
+	if vmbox.skipcheckpass:
+		agi.set_variable('XIVO_VMOPTIONS', "s")
+
+	agi.set_variable('XIVO_MAILBOX' vmbox.mailbox)
+	agi.set_variable('XIVO_MAILBOX_CONTEXT' vmbox.context)
+
+agid.register(vmbox_get_info)
