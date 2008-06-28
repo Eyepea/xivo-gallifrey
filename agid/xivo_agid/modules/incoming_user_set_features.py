2f57711ea017ab866705af17a9717acc820a7c09
Guillaume Knispel <gknispel@proformatique.com>
2008-06-28 14:26:14 +0000

    number = 100 makes no sense, removed
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@3446 dc85baef-7018-0410-bfbd-8a639da648bb

--- agid/xivo_agid/modules/incoming_user_set_features.py
+++ agid/xivo_agid/modules/incoming_user_set_features.py
@@ -36,7 +36,7 @@ def incoming_user_set_features(agi, cursor, args):
 	except LookupError:
 		caller = None
 
-	user = objects.User(agi, cursor, feature_list, xid = dstid, number = 100, context = context)
+	user = objects.User(agi, cursor, feature_list, xid = dstid, context = context)
 	filter = user.filter
 
 	# Special case. If a boss-secretary filter is set, the code will prematurely
