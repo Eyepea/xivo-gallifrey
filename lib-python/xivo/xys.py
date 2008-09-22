e3b9e3776a7fcc2f88abe0da60a6d8d17147b806
Guillaume Knispel <gknispel@proformatique.com>
2008-09-22 22:24:09 +0000

    [feature] parameterized validator now also works with symbol type strings [feature] added an "enum" standard parameterized validator [cleanup] pylint
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@4178 dc85baef-7018-0410-bfbd-8a639da648bb

--- lib-python/xivo/xys.py
+++ lib-python/xivo/xys.py
@@ -2,22 +2,23 @@
 
 Copyright (C) 2008  Proformatique
 
-The basic idea behind XYS is to write a schema as much as possible as you would
-write documents that are valid by this schema.
+The basic idea behind XYS is to write a schema as much as possible as
+you would write documents that are valid by this schema.
 
-If a mapping is needed at any level in documents, you just have to put one at
-the same place in the schema.  The keys in the schema are the ones that are
-allowed in documents.  If a key in the schema is a string that ends with a '?'
-then it is optional and when it appears in a document it does not end with this
-final '?'.  A value in the schema contains a sub-schema that will be used to
-validate each corresponding value in documents.
+If a mapping is needed at any level in documents, you just have to put
+one at the same place in the schema.  The keys in the schema are the
+ones that are allowed in documents.  If a key in the schema is a string
+that ends with a '?' then it is optional and when it appears in a
+document it does not end with this final '?'.  A value in the schema
+contains a sub-schema that will be used to validate each corresponding
+value in documents.
 
-This equivalence principle of types stands valid for sequences and scalars.  If
-a sequence is needed in documents, you write, in the schema, a sequence of only
-one element.  This element is a sub-schema that will be used to validate each
-element of each corresponding sequence in documents.  The type of a scalar in
-the schema is used to validate the type of each corresponding scalar in
-documents.
+This type equivalence principle stands valid for sequences and scalars.
+If a sequence is needed in documents, you write, in the schema, a
+sequence of only one element.  This element is a sub-schema that will be
+used to validate each element of each corresponding sequence in
+documents.  The type of a scalar in the schema is used to validate the
+type of each corresponding scalar in documents.
 
 Schema example:
 ---
@@ -62,18 +63,19 @@ peoples:
    - name: 10
 ...
 
-A typical feature of schema languages is the capability to describe usual
-subsets for scalars, for example it can be useful to declare that some integers
-in documents must be between 42 and 128.  This is done in XYS using
-personalized YAML tags.  Standard XYS qualifiers are provided; their tags
-starts with '!~~'.  An application can also define its own qualifiers with tags
-starting with '!~' (but not '!~~').  Some qualifying tags will have parameters,
-for example !~between(42,128) is a tag which means that corresponding integers
-in documents must be between 42 and 128.  Because of the YAML grammar,
-parameters of our !~~ and !~ tags must be integers (we excluded strings because
-only the left part of the tag up to a potential space character would have been
-considered), and there must be no space between the opening parenthesis and the
-closing one.  Qualifiers without parameter must be written with no parenthesis.
+A typical feature of schema languages is the capability to describe
+usual subsets for scalars, for example it can be useful to declare that
+some strings in documents must start with "fortytwo_".  This is done in
+XYS using personalized YAML tags.  Standard XYS qualifiers are provided;
+their tags starts with '!~~'.  An application can also define its own
+qualifiers with tags starting with '!~' (but not '!~~').  Some
+qualifying tags will have parameters, for example !~between(42,128) is a
+tag which means that corresponding integers in documents must be between
+42 and 128.  Because of the YAML grammar, these tags must be integers in
+decimal representation or symbols of the form /[A-Za-z_][A-Za-z0-9_]*/
+and there must be no space character between the opening parenthesis and
+the closing one.  Qualifiers without parameter must be written with no
+parenthesis.
 
 Example of schema with qualifiers:
 ---
@@ -93,8 +95,8 @@ Application-specific validation functions:
 
 import re
 
-def ipv4_address(docstr, schema):
-    elts = docstr.split('.', 4)
+def ipv4_address(nstr, schema):
+    elts = nstr.split('.', 4)
     if len(elts) != 4:
         return False
     for e in elts:
@@ -106,13 +108,13 @@ def ipv4_address(docstr, schema):
             return False
     return True
 
-def search_domain(docstr, schema):
+def search_domain(nstr, schema):
     domain_label_ok = \\
         re.compile(r'[a-zA-Z]([-a-zA-Z0-9]*[a-zA-Z0-9])?$').match
-    return docstr and len(docstr) <= 251 and \\
+    return nstr and len(nstr) <= 251 and \\
            all((((len(label) <= 63)
                  and domain_label_ok(label))
-                for label in docstr.split('.')))
+                for label in nstr.split('.')))
 
 
 Registration of above functions:
@@ -120,15 +122,17 @@ Registration of above functions:
 xys.add_validator(search_domain, u'!!str')
 xys.add_validator(ipv4_address, u'!!str')
 
-In this schema, most scalars are used just as an example of what can appear in
-a valid document.  An exception is for !~~prefixedDec static_, where static_ is
-used to check that in documents, keys start with static_ (with a decimal in
-their right part).  As you can see in the registration, qualifying XYS types
-derive from base YAML types.  The base types are used for two purposes: during
-construction of the internal representation of the schema, scalars are
-converted according to the base type specification; and when validating a
-document, the type of scalars it contains is checked against the specified base
-type prior to the call of the validation function.
+
+In this schema, most scalars are used just as an example of what can
+appear in a valid document.  An exception is for !~~prefixedDec static_,
+where static_ is used to check that in documents, keys start with
+static_ (with a decimal in their right part).  As you can see in the
+registration, qualifying XYS types derive from base YAML types.  The
+base types are used for two purposes: during construction of the
+internal representation of the schema, scalars are converted according
+to the base type specification; and when validating a document, the type
+of scalars it contains is checked against the specified base type prior
+to the call to the validation function.
 
 """
 
@@ -151,7 +155,8 @@ __license__ = """
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
-from xivo import UpCollections
+
+from xivo import UpCollections # pylint: disable-msg=W0611
 
 from collections import namedtuple
 import copy
@@ -159,7 +164,7 @@ import yaml
 import logging
 
 
-log = logging.getLogger("xivo.xys")
+log = logging.getLogger("xivo.xys") # pylint: disable-msg=C0103
 
 
 # NOTE: content must stay first
@@ -167,13 +172,21 @@ ValidatorNode = namedtuple('ValidatorNode', 'content validator')
 Optional = namedtuple('Optional', 'content')
 Mandatory = namedtuple('Mandatory', 'content')
 
-def get_content_optmand_val(x):
+
+def _optmand_content(x):
+    """
+    When a dictionary key is optional/mandatory, its corresponding
+    _value_ is decorated in an Optional/Mandatory tuple.
+    This function undo the decoration when necessary.
+    """
     if isinstance(x, Optional) or isinstance(x, Mandatory):
         return x.content
     else:
         return x
 
-def construct_node(loader, node, base_tag):
+
+def _construct_node(loader, node, base_tag):
+    "Warning: depends on python-yaml internals"
     node = copy.copy(node) # bypass YAML anti recursion
     best_tag = base_tag
     best_fit = 0
@@ -187,10 +200,21 @@ def construct_node(loader, node, base_tag):
     node.tag = best_tag
     return loader.construct_object(node, deep=True)
 
-def split_int_params(tag_prefix, tag_suffix):
+
+def _maybe_int(s):
+    "Coerces to int if starts with a digit else return s"
+    if s and s[0] in "0123456789":
+        return int(s)
+    else:
+        return s
+
+
+def _split_params(tag_prefix, tag_suffix):
+    "Split comma-separated tag_suffix[:-1] and map with _maybe_int"
     if tag_suffix[-1:] != ')':
         raise ValueError, "unbalanced parenthesis in type %s%s" % (tag_prefix, tag_suffix)
-    return map(int, tag_suffix[:-1].split(','))
+    return map(_maybe_int, tag_suffix[:-1].split(','))
+
 
 def add_validator(validator, base_tag, tag=None):
     """
@@ -209,45 +233,63 @@ def add_validator(validator, base_tag, tag=None):
         tag,
         lambda loader, node:
             ValidatorNode(
-                construct_node(loader, node, base_tag),
+                _construct_node(loader, node, base_tag),
                 validator))
+
 
 def add_parameterized_validator(param_validator, base_tag, tag_prefix=None):
     """
     Add a parameterized validator for the given tag prefix.
     If tag_prefix is None, it is automatically constructed as
     u'!~%s(' % param_validator.__name__
-    A parametrized validator is a function that accepts a document node (in
-    the form of a Python object) and a schema node (also a Python object)
-    and other integer parameters that directly come from its complete name
-    in the schema.  It returns True if the document node is valid according
-    to the schema node.  Note that the validator function does not have to
-    recurse in sub-nodes, because XYS already does it.
+    A parametrized validator is a function that accepts a document node
+    (in the form of a Python object), a schema node (also a Python
+    object), and other parameters (integer or string) that directly come
+    from its complete YAML name in the schema.  It returns True if the
+    document node is valid according to the schema node.  Note that the
+    validator function does not have to recurse in sub-nodes, because
+    XYS already does that.
     """
+    # pylint: disable-msg=C0111,W0621
     if not tag_prefix:
         tag_prefix = u'!~%s(' % param_validator.__name__
     def multi_constructor(loader, tag_suffix, node):
         def temp_validator(node, schema):
-            return param_validator(node, schema, *split_int_params(tag_prefix, tag_suffix))
+            return param_validator(node, schema, *_split_params(tag_prefix, tag_suffix))
         temp_validator.__name__ = str(tag_prefix + tag_suffix)
-        return ValidatorNode(construct_node(loader, node, base_tag), temp_validator)
+        return ValidatorNode(_construct_node(loader, node, base_tag), temp_validator)
     yaml.add_multi_constructor(tag_prefix, multi_constructor)
 
+
 def _add_validator_internal(validator, base_tag):
+    "with builtin tag prefixing"
     add_validator(validator, base_tag, tag = u'!~~' + validator.__name__)
 
+
 def _add_parameterized_validator_internal(param_validator, base_tag):
+    "with builtin tag prefixing"
     add_parameterized_validator(param_validator, base_tag, tag_prefix=u'!~~%s(' % param_validator.__name__)
 
-def seqlen(lst, schema, min_len, max_len):
+
+def enum(nstr, schema, *symbols): # pylint: disable-msg=W0613
+    """
+    !~~enum(symb1[,symb2[,...]])
+        corresponding strings in documents must be in the set of
+        given symbols.
+    """
+    return nstr in symbols
+
+
+def seqlen(lst, schema, min_len, max_len): # pylint: disable-msg=W0613
     """
     !~~seqlen(min,max)
-        corresponding sequences in documents must have a length between min
-        and max, included.
+        corresponding sequences in documents must have a length between
+        min and max, included.
     """
     return min_len <= len(lst) <= max_len
 
-def between(val, schema, min_val, max_val):
+
+def between(val, schema, min_val, max_val): # pylint: disable-msg=W0613
     """
     !~~between(min,max)
         corresponding integers in documents must be between min and max,
@@ -255,36 +297,42 @@ def between(val, schema, min_val, max_val):
     """
     return min_val <= val <= max_val
 
-def startswith(docstr, schema):
+
+def startswith(nstr, schema):
     """
     !~~startswith
-        corresponding strings in documents must begin with the associated
-        string in the schema.
+        corresponding strings in documents must begin with the
+        associated string in the schema.
     """
-    return docstr.startswith(schema)
+    return nstr.startswith(schema)
 
-def prefixedDec(docstr, schema):
+
+def prefixedDec(nstr, schema):
     """
     !~~prefixedDec
-        corresponding strings in documents must begin with the associated
-        string in the schema, and the right part of strings in documents
-        must be decimal.
+        corresponding strings in documents must begin with the
+        associated string in the schema, and the right part of strings
+        in documents must be decimal.
     """
-    if not docstr.startswith(schema):
+    if not nstr.startswith(schema):
         return False
-    postfix = docstr[len(schema):]
+    postfix = nstr[len(schema):]
     try:
         int(postfix)
     except ValueError:
         return False
     return True
 
+
 _add_parameterized_validator_internal(seqlen, u'!!seq')
 _add_parameterized_validator_internal(between, u'!!int')
+_add_parameterized_validator_internal(enum, u'!!str')
 _add_validator_internal(startswith, u'!!str')
 _add_validator_internal(prefixedDec, u'!!str')
 
-def qualify_map(key, content):
+
+def _qualify_map(key, content):
+    "see _optmand_content() pydoc"
     if isinstance(key, basestring):
         if key[-1:] == '?':
             return key[:-1], Optional(content)
@@ -293,28 +341,32 @@ def qualify_map(key, content):
     else:
         return key, content
 
-def transschema(x):
+
+def _transschema(x):
     """
     Transform a schema, once loaded from its YAML representation, to its
     final internal representation
     """
     if isinstance(x, tuple):
-        return x.__class__(transschema(x[0]), *x[1:])
+        return x.__class__(_transschema(x[0]), *x[1:])
     elif isinstance(x, dict):
-        return dict((qualify_map(key, transschema(val)) for key, val in x.iteritems()))
+        return dict((_qualify_map(key, _transschema(val)) for key, val in x.iteritems()))
     elif isinstance(x, list):
-        return map(transschema, x)
+        return map(_transschema, x)
     else:
         return x
+
 
 def load(src):
     """
     Parse the first XYS schema in a stream and produce the corresponding
     internal representation.
     """
-    return transschema(yaml.load(src))
+    return _transschema(yaml.load(src))
+
 
 Nothing = object()
+
 
 # TODO: display the document path to errors, and other error message enhancements
 # TODO: allow error messages from validators
@@ -341,12 +393,12 @@ def validate(document, schema):
         optional = {}
         mandatory = []
         for key, schema_val in schema.iteritems():
-            schema_val_content = get_content_optmand_val(schema_val)
+            schema_val_content = _optmand_content(schema_val)
             if isinstance(key, ValidatorNode):
                 generic.append((key, schema_val_content))
             elif isinstance(schema_val, Optional):
                 optional[key] = schema_val_content
-            else: # Mandatory
+            else:
                 mandatory.append((key, schema_val_content))
         doc_copy = document.copy()
         for key, schema_val in mandatory:
@@ -387,12 +439,14 @@ def validate(document, schema):
             return False
         return True
 
+
 __all__ = [
         'validate', 'load',
         'seqlen', 'between', 'startswith', 'prefixedDec',
         'add_validator', 'add_parameterized_validator',
         'ValidatorNode', 'Optional', 'Mandatory',
 ]
+
 
 # IDEAS:
 # 04:05 < obk> xilun: You use '?' for optional... do you use '*' and '?' for zero-or-more and one-or-more (in sequences)?
