85c5abfb5b24b5ed9ce1e34a759d8819695a082e
Adrien DELLE CAVE (Decryptus) <decryptus@proformatique.com>
2008-10-21 01:46:18 +0000

    [interface] Renamed calldate to eventdate
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@4402 dc85baef-7018-0410-bfbd-8a639da648bb

--- ctiservers/init/datastorage/mysql/xivo.db.sql
+++ ctiservers/init/datastorage/mysql/xivo.db.sql
@@ -7,7 +7,7 @@ USE `xivo`;
 
 DROP TABLE IF EXISTS `ctilog`;
 CREATE TABLE `ctilog` (
- `calldate` timestamp DEFAULT 0,
+ `eventdate` timestamp DEFAULT 0,
  `loginclient` varchar(64),
  `company` varchar(64),
  `status` varchar(32),
@@ -15,7 +15,7 @@ CREATE TABLE `ctilog` (
  `arguments` varchar(255) NOT NULL
 );
 
-CREATE INDEX `ctilog__idx__calldate` ON `ctilog`(`calldate`);
+CREATE INDEX `ctilog__idx__eventdate` ON `ctilog`(`eventdate`);
 CREATE INDEX `ctilog__idx__loginclient` ON `ctilog`(`loginclient`);
 CREATE INDEX `ctilog__idx__company` ON `ctilog`(`company`);
 CREATE INDEX `ctilog__idx__status` ON `ctilog`(`status`);
--- ctiservers/init/datastorage/sqlite/xivo.db.sql
+++ ctiservers/init/datastorage/sqlite/xivo.db.sql
@@ -2,7 +2,7 @@ BEGIN TRANSACTION;
 
 DROP TABLE ctilog;
 CREATE TABLE ctilog (
- calldate char(19) DEFAULT '0000-00-00 00:00:00',
+ eventdate char(19) DEFAULT '0000-00-00 00:00:00',
  loginclient varchar(64),
  company varchar(64),
  status varchar(32),
@@ -10,7 +10,7 @@ CREATE TABLE ctilog (
  arguments varchar(255) NOT NULL
 );
 
-CREATE INDEX ctilog__idx__calldate ON ctilog(calldate);
+CREATE INDEX ctilog__idx__eventdate ON ctilog(eventdate);
 CREATE INDEX ctilog__idx__loginclient ON ctilog(loginclient);
 CREATE INDEX ctilog__idx__company ON ctilog(company);
 CREATE INDEX ctilog__idx__status ON ctilog(status);
