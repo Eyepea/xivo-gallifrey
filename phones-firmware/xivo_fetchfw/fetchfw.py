dde6b7aca1cf66530a50a959b6ff5b98220a9bc0
Guillaume Knispel <gknispel@proformatique.com>
2008-10-27 19:51:16 +0000

    [cleanup] indentation and convention on some symbol names, plus few pylint annotations
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@4517 dc85baef-7018-0410-bfbd-8a639da648bb

--- phones-firmware/xivo_fetchfw/fetchfw.py
+++ phones-firmware/xivo_fetchfw/fetchfw.py
@@ -25,7 +25,7 @@ import shutil
 import urllib2
 import subprocess
 import ConfigParser
-from stat import *
+from stat import S_IRWXU, S_IRUSR, S_IWUSR, S_IRGRP, S_IXGRP, S_IROTH, S_IXOTH
 
 CONFIG_FILE = "/etc/pf-xivo/fetchfw.conf"
 CONFIG_SECTION_GENERAL = "general"
@@ -43,231 +43,231 @@ brands = {}
 # (name -> object) dictionnary.
 firmwares = {}
 
-class remote_file:
-	"""This class is used to gather some properties about a remote file
-	accessible through a protocol supported by the urllib2 module and
-	download it. The properties are given when a remote_file object is
-	instantiated. The user can then call the fetch() method to actually
-	download the remote file into the tftp_path directory. The file can
-	be cached, in which case there is of course no download. The
-	fetch() method always performs size/hash checking, even if the file
-	was cached.
-	"""
+class RemoteFile:
+    """This class is used to gather some properties about a remote file
+    accessible through a protocol supported by the urllib2 module and
+    download it. The properties are given when a RemoteFile object is
+    instantiated. The user can then call the fetch() method to actually
+    download the remote file into the tftp_path directory. The file can
+    be cached, in which case there is of course no download. The
+    fetch() method always performs size/hash checking, even if the file
+    was cached.
+    """
 
-	BUFFER_SIZE = 8192
+    BUFFER_SIZE = 8192
 
-	def __init__(self, filename, url, size, md5sum, sha1sum):
-		global tmp_path
+    def __init__(self, filename, url, size, md5sum, sha1sum):
+        global tmp_path
 
-		self.filename = filename
-		self.url = url
-		self.size = size
-		self.md5sum = md5sum
-		self.sha1sum = sha1sum
-		self.path = os.path.join(tmp_path, filename)
+        self.filename = filename
+        self.url = url
+        self.size = size
+        self.md5sum = md5sum
+        self.sha1sum = sha1sum
+        self.path = os.path.join(tmp_path, filename)
 
-	def fetch(self):
-		md_md5 = md5.new()
-		md_sha = sha.new()
-		size = 0
+    def fetch(self):
+        md_md5 = md5.new()
+        md_sha = sha.new()
+        size = 0
 
-		try:
-			src = open(self.path)
-			dst = None
-		except IOError:
-			src = urllib2.urlopen(self.url)
-			dst = open(self.path, "w")
+        try:
+            src = open(self.path)
+            dst = None
+        except IOError:
+            src = urllib2.urlopen(self.url)
+            dst = open(self.path, "w")
 
-		while True:
-			buffer = src.read(remote_file.BUFFER_SIZE)
+        while True:
+            buf = src.read(RemoteFile.BUFFER_SIZE)
 
-			if not buffer:
-				break
+            if not buf:
+                break
 
-			md_md5.update(buffer)
-			md_sha.update(buffer)
-			size += len(buffer)
+            md_md5.update(buf)
+            md_sha.update(buf)
+            size += len(buf)
 
-			if dst:
-				dst.write(buffer)
+            if dst:
+                dst.write(buf)
 
-		src.close()
+        src.close()
 
-		if dst:
-			dst.close()
+        if dst:
+            dst.close()
 
-		def die_dont_match(what, filename):
-			print "error: %s doesn't match for file %s" % (what, filename)
-			sys.exit()
+        def die_dont_match(what, filename):
+            print "error: %s doesn't match for file %s" % (what, filename)
+            sys.exit()
 
-		if md_md5.hexdigest() != self.md5sum:
-			die_dont_match("MD5 sum", self.filename)
+        if md_md5.hexdigest() != self.md5sum:
+            die_dont_match("MD5 sum", self.filename)
 
-		if md_sha.hexdigest() != self.sha1sum:
-			die_dont_match("SHA-1 sum", self.filename)
+        if md_sha.hexdigest() != self.sha1sum:
+            die_dont_match("SHA-1 sum", self.filename)
 
-		if size != self.size:
-			die_dont_match("size", self.filename)
+        if size != self.size:
+            die_dont_match("size", self.filename)
 
 class firmware:
-	"""This class is used to store firmware properties and automatically
-	select the appropriate installation function. Properties are given
-	when a firmware object is created, and the user can later call the
-	install() method to get the required files and install the firmware.
-	"""
-	def __init__(self, name, brand, model, version, remote_files, description = None):
-		global install_fns
+    """This class is used to store firmware properties and automatically
+    select the appropriate installation function. Properties are given
+    when a firmware object is created, and the user can later call the
+    install() method to get the required files and install the firmware.
+    """
+    def __init__(self, name, brand, model, version, remote_files, description = None):
+        global install_fns
 
-		self.name = name
-		self.brand = brand
-		self.model = model
-		self.version = version
+        self.name = name
+        self.brand = brand
+        self.model = model
+        self.version = version
 
-		if description == None:
-			self.description = "%s %s %s firmware" % (brand, model, version)
-		else:
-			self.description = description
+        if description == None:
+            self.description = "%s %s %s firmware" % (brand, model, version)
+        else:
+            self.description = description
 
-		if model in install_fns[brand]:
-			self.install_fn = install_fns[brand][model]
-		else:
-			self.install_fn = install_fns[brand]['__default__']
+        if model in install_fns[brand]:
+            self.install_fn = install_fns[brand][model]
+        else:
+            self.install_fn = install_fns[brand]['__default__']
 
-		self.remote_files = remote_files
+        self.remote_files = remote_files
 
-	def install(self):
-		for remote_file in self.remote_files:
-			remote_file.fetch()
+    def install(self):
+        for remote_file in self.remote_files:
+            remote_file.fetch()
 
-		return self.install_fn(self)
+        return self.install_fn(self)
 
 def warn(message):
-	sys.stderr.write("warning: %s.\n" % message)
+    sys.stderr.write("warning: %s.\n" % message)
 
 def die(message):
-	sys.stderr.write("error: %s.\n" % message)
-	sys.exit(1)
+    sys.stderr.write("error: %s.\n" % message)
+    sys.exit(1)
 
 def register_install_fn(brand, model, install_fn):
-	"""Register install_fn for the given brand/model pair. If model is
-	None, install_fn applies to all models of the given brand.
-	"""
+    """Register install_fn for the given brand/model pair. If model is
+    None, install_fn applies to all models of the given brand.
+    """
 
-	global install_fns
+    global install_fns
 
-	if not model:
-		model = "__default__"
+    if not model:
+        model = "__default__"
 
-	if brand not in install_fns:
-		install_fns[brand] = {}
+    if brand not in install_fns:
+        install_fns[brand] = {}
 
-	if model in install_fns[brand]:
-		warn("multiple registrations for %s %s" % (brand, model))
+    if model in install_fns[brand]:
+        warn("multiple registrations for %s %s" % (brand, model))
 
-	install_fns[brand][model] = install_fn
+    install_fns[brand][model] = install_fn
 
 def fix_permissions(path):
-	"""Set 755/644 permisions recursively on the given path to ensure
-	extracted files are usable.
-	"""
-	dir_mode = S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH
-	file_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
-	os.chmod(path, dir_mode)
+    """Set 755/644 permisions recursively on the given path to ensure
+    extracted files are usable.
+    """
+    dir_mode = S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH
+    file_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
+    os.chmod(path, dir_mode)
 
-	for root, dirs, files, in os.walk(path):
-		for name in dirs:
-			os.chmod(os.path.join(root, name), dir_mode)
+    for root, dirs, files, in os.walk(path):
+        for name in dirs:
+            os.chmod(os.path.join(root, name), dir_mode)
 
-		for name in files:
-			os.chmod(os.path.join(root, name), file_mode)
+        for name in files:
+            os.chmod(os.path.join(root, name), file_mode)
 
 def zip_extract_all(label, zipfile_path):
-	"""Extract the content of zipfile_path into tmp_path/label and
-	return this path. This function completely removes the destination
-	directory before extracting files. label is used as a unique
-	identifier and usually contains the firmware name as a prefix.
-	"""
+    """Extract the content of zipfile_path into tmp_path/label and
+    return this path. This function completely removes the destination
+    directory before extracting files. label is used as a unique
+    identifier and usually contains the firmware name as a prefix.
+    """
 
-	zip_path = os.path.join(tmp_path, label)
-	shutil.rmtree(zip_path, True)
-	os.mkdir(zip_path)
+    zip_path = os.path.join(tmp_path, label)
+    shutil.rmtree(zip_path, True)
+    os.mkdir(zip_path)
 
-	try:
-		result = subprocess.call(['unzip', '-q', zipfile_path], cwd = zip_path, close_fds = True)
-		fix_permissions(zip_path)
+    try:
+        result = subprocess.call(['unzip', '-q', zipfile_path], cwd = zip_path, close_fds = True)
+        fix_permissions(zip_path)
 
-		if result:
-			die("zip extraction failed")
-	except OSError, e:
-		die("zip extraction failed: %s" % e)
+        if result:
+            die("zip extraction failed")
+    except OSError, e:
+        die("zip extraction failed: %s" % e)
 
-	return zip_path
+    return zip_path
 
 def tar_extract_all(label, tarfile_path):
-	"""Extract the content of tarfile_path into tmp_path/label and
-	return this path. This function completely removes the destination
-	directory before extracting files. label is used as a unique
-	identifier and usually contains the firmware name as a prefix.
-	"""
+    """Extract the content of tarfile_path into tmp_path/label and
+    return this path. This function completely removes the destination
+    directory before extracting files. label is used as a unique
+    identifier and usually contains the firmware name as a prefix.
+    """
 
-	tar_path = os.path.join(tmp_path, label)
-	shutil.rmtree(tar_path, True)
-	os.mkdir(tar_path)
+    tar_path = os.path.join(tmp_path, label)
+    shutil.rmtree(tar_path, True)
+    os.mkdir(tar_path)
 
-	try:
-		result = subprocess.call(['tar', 'xf', tarfile_path], cwd = tar_path, close_fds = True)
-		fix_permissions(tar_path)
+    try:
+        result = subprocess.call(['tar', 'xf', tarfile_path], cwd = tar_path, close_fds = True)
+        fix_permissions(tar_path)
 
-		if result:
-			die("tar extraction failed")
-	except OSError, e:
-		die("tar extraction failed: %s" % e)
+        if result:
+            die("tar extraction failed")
+    except OSError, e:
+        die("tar extraction failed: %s" % e)
 
-	return tar_path
+    return tar_path
 
 def tgz_extract_all(label, tgzfile_path):
-	"""Extract the content of tgzfile_path into tmp_path/label and
-	return this path. This function completely removes the destination
-	directory before extracting files. label is used as a unique
-	identifier and usually contains the firmware name as a prefix.
-	"""
+    """Extract the content of tgzfile_path into tmp_path/label and
+    return this path. This function completely removes the destination
+    directory before extracting files. label is used as a unique
+    identifier and usually contains the firmware name as a prefix.
+    """
 
-	tgz_path = os.path.join(tmp_path, label)
-	shutil.rmtree(tgz_path, True)
-	os.mkdir(tgz_path)
+    tgz_path = os.path.join(tmp_path, label)
+    shutil.rmtree(tgz_path, True)
+    os.mkdir(tgz_path)
 
-	try:
-		result = subprocess.call(['tar', 'xzf', tgzfile_path], cwd = tgz_path, close_fds = True)
-		fix_permissions(tgz_path)
+    try:
+        result = subprocess.call(['tar', 'xzf', tgzfile_path], cwd = tgz_path, close_fds = True)
+        fix_permissions(tgz_path)
 
-		if result:
-			die("tgz extraction failed")
-	except OSError, e:
-		die("tgz extraction failed: %s" % e)
+        if result:
+            die("tgz extraction failed")
+    except OSError, e:
+        die("tgz extraction failed: %s" % e)
 
-	return tgz_path
+    return tgz_path
 
 def tbz2_extract_all(label, tbz2file_path):
-	"""Extract the content of tbz2file_path into tmp_path/label and
-	return this path. This function completely removes the destination
-	directory before extracting files. label is used as a unique
-	identifier and usually contains the firmware name as a prefix.
-	"""
+    """Extract the content of tbz2file_path into tmp_path/label and
+    return this path. This function completely removes the destination
+    directory before extracting files. label is used as a unique
+    identifier and usually contains the firmware name as a prefix.
+    """
 
-	tbz2_path = os.path.join(tmp_path, label)
-	shutil.rmtree(tbz2_path, True)
-	os.mkdir(tbz2_path)
+    tbz2_path = os.path.join(tmp_path, label)
+    shutil.rmtree(tbz2_path, True)
+    os.mkdir(tbz2_path)
 
-	try:
-		result = subprocess.call(['tar', 'xjf', tbz2file_path], cwd = tbz2_path, close_fds = True)
-		fix_permissions(tbz2_path)
+    try:
+        result = subprocess.call(['tar', 'xjf', tbz2file_path], cwd = tbz2_path, close_fds = True)
+        fix_permissions(tbz2_path)
 
-		if result:
-			die("tbz2 extraction failed")
-	except OSError, e:
-		die("tbz2 extraction failed: %s" % e)
+        if result:
+            die("tbz2 extraction failed")
+    except OSError, e:
+        die("tbz2 extraction failed: %s" % e)
 
-	return tbz2_path
+    return tbz2_path
 
 config = ConfigParser.RawConfigParser()
 fp = open(CONFIG_FILE)
@@ -282,55 +282,55 @@ fp = open(firmwares_db_path)
 config.readfp(fp)
 
 try:
-	os.makedirs(tmp_path)
+    os.makedirs(tmp_path)
 except OSError:
-	pass
+    pass # XXX: catching every OSError is not appropriate
 
-import aastra
-import linksys
-import polycom
-import snom
-import thomson
-import digium
+from xivo_fetchfw import aastra         # pylint: disable-msg=W0611
+from xivo_fetchfw import linksys        # pylint: disable-msg=W0611
+from xivo_fetchfw import polycom        # pylint: disable-msg=W0611
+from xivo_fetchfw import snom           # pylint: disable-msg=W0611
+from xivo_fetchfw import thomson        # pylint: disable-msg=W0611
+from xivo_fetchfw import digium         # pylint: disable-msg=W0611
 
 fw_names = config.sections()
 
 for fw_name in fw_names:
-	if not config.has_option(fw_name, "brand"):
-		continue
+    if not config.has_option(fw_name, "brand"):
+        continue
 
-	brand = config.get(fw_name, "brand")
-	model = config.get(fw_name, "model")
-	version = config.get(fw_name, "version")
-	files = config.get(fw_name, "files").split()
-	remote_files = []
+    brand = config.get(fw_name, "brand")
+    model = config.get(fw_name, "model")
+    version = config.get(fw_name, "version")
+    files = config.get(fw_name, "files").split()
+    remote_files = []
 
-	for file in files:
-		url = config.get(file, "url")
-		size = config.getint(file, "size")
-		md5sum = config.get(file, "md5sum")
-		sha1sum = config.get(file, "sha1sum")
-		remote_files.append(remote_file(file, url, size, md5sum, sha1sum))
+    for xfile in files:
+        url = config.get(xfile, "url")
+        size = config.getint(xfile, "size")
+        md5sum = config.get(xfile, "md5sum")
+        sha1sum = config.get(xfile, "sha1sum")
+        remote_files.append(RemoteFile(xfile, url, size, md5sum, sha1sum))
 
-	if config.has_option(fw_name, "description"):
-		description = config.get(fw_name, "description")
-	else:
-		description = None
+    if config.has_option(fw_name, "description"):
+        description = config.get(fw_name, "description")
+    else:
+        description = None
 
-	fw = firmware(fw_name, brand, model, version, remote_files, description)
+    fw = firmware(fw_name, brand, model, version, remote_files, description)
 
-	if brand not in brands:
-		brands[brand] = {}
+    if brand not in brands:
+        brands[brand] = {}
 
-	if model not in brands[brand]:
-		brands[brand][model] = {}
+    if model not in brands[brand]:
+        brands[brand][model] = {}
 
-	if version in brands[brand][model]:
-		warn("multiple registrations for %s %s %s" % (brand, model, version))
+    if version in brands[brand][model]:
+        warn("multiple registrations for %s %s %s" % (brand, model, version))
 
-	brands[brand][model][version] = fw
+    brands[brand][model][version] = fw
 
-	if fw_name in firmwares:
-		warn("multiple registrations for firmware %s" % fw_name)
+    if fw_name in firmwares:
+        warn("multiple registrations for firmware %s" % fw_name)
 
-	firmwares[fw_name] = fw
+    firmwares[fw_name] = fw
