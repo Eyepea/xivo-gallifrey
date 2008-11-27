4984d8bcdfe4f349717d72468866313bd4353fa4
Corentin Le Gall <clegall@proformatique.com>
2008-11-27 15:58:07 +0000

    french language corrections
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@4791 dc85baef-7018-0410-bfbd-8a639da648bb

--- asternic-stats/html/lang/fr.php
+++ asternic-stats/html/lang/fr.php
@@ -48,7 +48,7 @@ $lang['fr']['menu_cti'] = "CTI";
 // tooltips
 $lang['fr']['pdfhelp'] = "Exporter les données dans un fichier .pdf";
 $lang['fr']['csvhelp'] = "Exporter les données dans un fichier avec un séparateur afin de les lire dans un tableur";
-$lang['fr']['gotop']   = "Allez au début de la page";
+$lang['fr']['gotop']   = "Aller au début de la page";
 
 // Index page
 $lang['fr']['ALL']               = "TOUS";
@@ -56,7 +56,7 @@ $lang['fr']['lower']             = "Plus petit  ...";
 $lang['fr']['higher']            = "Plus grand ...";
 $lang['fr']['select_queue']      = "Séléctionner une file";
 $lang['fr']['select_agent']      = "Séléctionner un agent";
-$lang['fr']['select_timeframe']  = "Séléctionner un interval de temps";
+$lang['fr']['select_timeframe']  = "Séléctionner un intervalle de temps";
 $lang['fr']['queue']   	         = "File";
 $lang['fr']['start']   	         = "Date de début";
 $lang['fr']['end']   	         = "Date de fin";
@@ -68,16 +68,16 @@ $lang['fr']['this_month']        = "Ce mois";
 $lang['fr']['last_three_months'] = "Derniers 3 mois";
 $lang['fr']['available']         = "Disponible";
 $lang['fr']['selected']          = "Selectionné";
-$lang['fr']['invaliddate']       = "Interval des dates invalides";
+$lang['fr']['invaliddate']       = "Intervalle des dates invalides";
 
 // Answered page
-$lang['fr']['answered_calls_by_agent'] = "Appel répondu par agent";
-$lang['fr']['answered_calls_by_queue'] = "Appel répondu par file d\'attente";
-$lang['fr']['anws_unanws_by_hour']     = "Répondu/non-répondu par heure";
+$lang['fr']['answered_calls_by_agent'] = "Appels répondus par agent";
+$lang['fr']['answered_calls_by_queue'] = "Appels répondus par file d\'attente";
+$lang['fr']['anws_unanws_by_hour']     = "Répondus/non-répondus par heure";
 $lang['fr']['report_info']       = "Information des rapports";
 $lang['fr']['period']            = "Période";
-$lang['fr']['answered_calls']    = "Appel répondu";
-$lang['fr']['transferred_calls'] = "Appel transférer";
+$lang['fr']['answered_calls']    = "Appels répondus";
+$lang['fr']['transferred_calls'] = "Appels transférés";
 $lang['fr']['secs']              = "secs";
 $lang['fr']['minutes']           = "min";
 $lang['fr']['hours']             = "hs";
@@ -107,26 +107,26 @@ $lang['fr']['transfers']         = "Transfert";
 $lang['fr']['to']                = "Pour";
 
 // Unanswered page
-$lang['fr']['unanswered_calls']    = "Appel non répondu";
-$lang['fr']['number_unanswered']   = "Nombre d'appels non répondu";
-$lang['fr']['number_totals']   = "Nombre d'appels total présentés";
+$lang['fr']['unanswered_calls']    = "Appels non répondus";
+$lang['fr']['number_unanswered']   = "Nombre d'appels non répondus";
+$lang['fr']['number_totals']   = "Nombre d'appels totaux présentés";
 $lang['fr']['avg_wait_before_dis'] = "Moy. du temps d'attente avant le raccroché";
 $lang['fr']['avg_queue_pos_at_dis']= "Moy. de la position dans la file lors de la déconnexion";
 $lang['fr']['avg_queue_start']     = "Moy. lors de l'arrivé dans la file";
 $lang['fr']['user_abandon']        = "Abandon des utilisateurs";
 $lang['fr']['abandon']             = "Abandon";
 $lang['fr']['timeout']             = "Délai dépassé";
-$lang['fr']['unanswered_calls_qu'] = "Appel non-répondu par file";
+$lang['fr']['unanswered_calls_qu'] = "Appels non-répondus par file";
 
 // Distribution
-$lang['fr']['totals']              = "Totals";
-$lang['fr']['number_answered']     = "Nombre d'appel répondu";
-$lang['fr']['number_unanswered']   = "Nombre d'appel non répondu";
+$lang['fr']['totals']              = "Totaux";
+$lang['fr']['number_answered']     = "Nombre d'appels répondus";
+$lang['fr']['number_unanswered']   = "Nombre d'appels non répondus";
 $lang['fr']['agent_login']         = "Agent loggué";
 $lang['fr']['agent_logoff']        = "Agent déloggué";
 $lang['fr']['call_distrib_day']    = "Répartition d'appels par jour";
-$lang['fr']['call_distrib_hour']   = "Répartition d'appel par heure";
-$lang['fr']['call_distrib_week']   = "Répartition d'appel par jour de semaine";
+$lang['fr']['call_distrib_hour']   = "Répartition d'appels par heure";
+$lang['fr']['call_distrib_week']   = "Répartition d'appels par jour de semaine";
 $lang['fr']['date']                = "Date";
 $lang['fr']['day']                 = "Jour";
 $lang['fr']['days']                = "jours";
@@ -137,14 +137,14 @@ $lang['fr']['percent_answered']    = "% répondu";
 $lang['fr']['percent_unanswered']  = "% non répondu";
 $lang['fr']['login']               = "Loggué";
 $lang['fr']['logoff']              = "Déloggué";
-$lang['fr']['answ_by_day']         = "Appels répondu par jour de la semaine";
-$lang['fr']['unansw_by_day']       = "Appels non répondu par jour de la semaine";
-$lang['fr']['avg_call_time_by_day']= "Moyenne du temps d\'appels pour jour de la semaine";
-$lang['fr']['avg_hold_time_by_day']= "Moyenne du temps d\'attente par jour de la semaine";
-$lang['fr']['answ_by_hour']        = "Appels répondu par heure";
-$lang['fr']['unansw_by_hour']      = "Appel non répondu par heure";
-$lang['fr']['avg_call_time_by_hr'] = "Moyenne du temps d\'appel par heure";
-$lang['fr']['avg_hold_time_by_hr'] = "Moyenne du temps d\'attente par heure";
+$lang['fr']['answ_by_day']         = "Appels répondus par jour de la semaine";
+$lang['fr']['unansw_by_day']       = "Appels non répondus par jour de la semaine";
+$lang['fr']['avg_call_time_by_day']= "Moyenne des temps d\'appel par jour de la semaine";
+$lang['fr']['avg_hold_time_by_day']= "Moyenne des temps d\'attente par jour de la semaine";
+$lang['fr']['answ_by_hour']        = "Appels répondus par heure";
+$lang['fr']['unansw_by_hour']      = "Appels non répondus par heure";
+$lang['fr']['avg_call_time_by_hr'] = "Moyenne des temps d\'appel par heure";
+$lang['fr']['avg_hold_time_by_hr'] = "Moyenne des temps d\'attente par heure";
 $lang['fr']['page']                = "Page";
 $lang['fr']['export']              = "Exporter la table:";
 
@@ -156,7 +156,7 @@ $lang['fr']['realtime_callwaiting_queue']		= "File";
 $lang['fr']['realtime_callwaiting_position']		= "Position";
 $lang['fr']['realtime_callwaiting_callerid']		= "Nom appelant";
 $lang['fr']['realtime_callwaiting_waitime']		= "Temps attente";
-$lang['fr']['realtime_agentstatus']			= "Status des agents";
+$lang['fr']['realtime_agentstatus']			= "Statut des agents";
 $lang['fr']['realtime_agentstatus_queue']		= "File";
 $lang['fr']['realtime_agentstatus_agent']		= "Agent";
 $lang['fr']['realtime_agentstatus_state']		= "État";
@@ -168,8 +168,8 @@ $lang['fr']['realtime_agent_summary_queue'] 		= "Files";
 $lang['fr']['realtime_agent_summary_staffed'] 		= "En ligne";
 $lang['fr']['realtime_agent_summary_talking'] 		= "En communication";
 $lang['fr']['realtime_agent_summary_paused'] 		= "En pause";
-$lang['fr']['realtime_agent_summary_callswaiting'] 	= "Appel en attente";
-$lang['fr']['realtime_agent_summary_oldcallwaiting']	= "Ancien appel en attente";
+$lang['fr']['realtime_agent_summary_callswaiting'] 	= "Appels en attente";
+$lang['fr']['realtime_agent_summary_oldcallwaiting']	= "Anciens appels en attente";
 
 
 ?>
