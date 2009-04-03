ab4df9fdd02f33907bbf3e674f765d0297e67bfc
Corentin Le Gall <clegall@proformatique.com>
2009-04-03 11:50:15 +0000

    updated XIVO licence header
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@5867 dc85baef-7018-0410-bfbd-8a639da648bb

--- ctiservers/xivo_ctiservers/client_connection.py
+++ ctiservers/xivo_ctiservers/client_connection.py
@@ -1,6 +1,27 @@
-# (c) Proformatique 2009
-# Thomas Bernard
-# non blocking socket
+# XIVO Daemon
+
+__version__   = '$Revision$'
+__date__      = '$Date$'
+__copyright__ = 'Copyright (C) 2009 Proformatique'
+__author__    = 'Thomas Bernard'
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Alternatively, XIVO Daemon is available under other licenses directly
+# contracted with Pro-formatique SARL. See the LICENSE file at top of the
+# source tree or delivered in the installable package in which XIVO Daemon
+# is distributed for more details.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import socket
 import errno
