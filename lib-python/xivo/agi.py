07948d34a4f5664af8e2ec9cb6afe40e93f137f0
Marc Dequènes (Duck) <mdequenes@proformatique.com>
2009-05-12 13:34:30 +0000

    [evol] License update to LGPL 3.0 where appropriate
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@6212 dc85baef-7018-0410-bfbd-8a639da648bb

--- lib-python/xivo/agi.py
+++ lib-python/xivo/agi.py
@@ -35,8 +35,6 @@ __license__ = """
     Copyright (C) 2004 Karl Putland 
     Upstream Original Author: Karl Putland <kputland@users.sourceforge.net>
 
-    Copyright (C) 2007, 2008, 2009  Proformatique
-
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
     License as published by the Free Software Foundation; either
@@ -50,6 +48,22 @@ __license__ = """
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+    Copyright (C) 2007, 2008, 2009  Proformatique
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 # Modifications by Proformatique from pyst-0.2:
