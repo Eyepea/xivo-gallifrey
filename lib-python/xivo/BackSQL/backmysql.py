be9c6b3370ea0f88121d38de366859530777815d
Marc Dequènes (Duck) <mdequenes@proformatique.com>
2009-05-12 13:38:24 +0000

    [evol] add proper permission to link with MySQL to the anysql MySQL backend.
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@6214 dc85baef-7018-0410-bfbd-8a639da648bb

--- lib-python/xivo/BackSQL/backmysql.py
+++ lib-python/xivo/BackSQL/backmysql.py
@@ -11,7 +11,15 @@ __license__ = """
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    (at your option) any later version with a Section 7 Additional
+    Permission as follows:
+      This notice constitutes a grant of such permission as is necessary
+      to combine or link this software, or a modified version of it, with
+      the MySQL Client Library, as published by Sun Microsystems and/or
+      MySQL AB, or a derivative work of the MySQL CLient LIbrary, and to
+      copy, modify, and distribute the resulting work. The MySQL Client
+      LIbrary is licensed under version 2 of the GNU General Public
+      License.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
