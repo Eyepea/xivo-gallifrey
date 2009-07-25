7bdfc5e544e3a50dda60087c67a7a19c20177c46
Guillaume Knispel <gknispel@proformatique.com>
2009-07-25 22:27:25 +0000

    [bugfix] Revert first change of commit 6695, because it broke the invariant urisup.uri_help_split(urisup.uri_help_unsplit(x)) == x.  Forbid the pathological situation in uri_tree_precode_check() and call it after uri_tree_normalize() in uri_help_unsplit() so that it properly detect that cas given the added test. (uri_tree_normalize() does not care if the uri_tree is percent encoded or not.)
    [cleanup] various cleanups
    
    
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@6729 dc85baef-7018-0410-bfbd-8a639da648bb

--- lib-python/xivo/urisup.py
+++ lib-python/xivo/urisup.py
@@ -23,35 +23,34 @@ __license__ = """
 """
 
 import re
-import string # pylint: disable-msg=W0402
 
 # Right from RFC 3986 section B
-RFC3986_MATCHER = re.compile('^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?').match
+RFC3986_MATCHER = re.compile(r"^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?").match
 
 # Near RFC 3986 definitions
-ALPHA = string.ascii_letters
-DIGIT = string.digits
-HEXDIG = string.digits + "abcdefABCDEF"
-SCHEME_CHAR = ALPHA + DIGIT + '+-.'
-GEN_DELIMS = ':/?#[]@'
+ALPHA = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+DIGIT = "0123456789"
+HEXDIG = "0123456789abcdefABCDEF"
+SCHEME_CHAR = ALPHA + DIGIT + "+-."
+GEN_DELIMS = ":/?#[]@"
 SUB_DELIMS = "!$&'()*+,;="
 RESERVED = GEN_DELIMS + SUB_DELIMS
-UNRESERVED = ALPHA + DIGIT + '-._~'
-PCHAR = UNRESERVED + SUB_DELIMS + '%:@/'
-QUEFRAG_CHAR = PCHAR + '?'
-USER_CHAR = UNRESERVED + SUB_DELIMS + '%'
-PASSWD_CHAR = UNRESERVED + SUB_DELIMS + '%:'
-REG_NAME_CHAR = UNRESERVED + SUB_DELIMS + '%'
+UNRESERVED = ALPHA + DIGIT + "-._~"
+PCHAR = UNRESERVED + SUB_DELIMS + "%:@/"
+QUEFRAG_CHAR = PCHAR + "?"
+USER_CHAR = UNRESERVED + SUB_DELIMS + "%"
+PASSWD_CHAR = UNRESERVED + SUB_DELIMS + "%:"
+REG_NAME_CHAR = UNRESERVED + SUB_DELIMS + "%"
 
-IPV_FUTURE_RE = 'v[\da-fA-F]+\.[' \
-                + re.escape(UNRESERVED + SUB_DELIMS + ':') \
-                + ']+'
+IPV_FUTURE_RE = r"v[\da-fA-F]+\.[" \
+                + re.escape(UNRESERVED + SUB_DELIMS + ":") \
+                + "]+"
 
 BYTES_VAL = ''.join(map(chr, range(0, 256)))
 
 def __allow_to_encdct(charset):
     enc_charset = set(iter(charset.replace('%','')))
-    return dict(((k in enc_charset) and (k, k) or (k, '%%%02X' % ord(k)) for k in BYTES_VAL))
+    return dict(((k in enc_charset) and (k, k) or (k, "%%%02X" % ord(k)) for k in BYTES_VAL))
 
 USER_ENCDCT = __allow_to_encdct(USER_CHAR)
 PASSWD_ENCDCT = __allow_to_encdct(PASSWD_CHAR)
@@ -60,8 +59,8 @@ P_ENCDCT = __allow_to_encdct(PCHAR)
 FRAG_ENCDCT = __allow_to_encdct(QUEFRAG_CHAR)
 # QUERY_ENCDCT is reserved for query encoding, that is spaces are not percent
 # encoded but translated into plus ' ' -> '+'
-QUERY_KEY_ENCDCT = __allow_to_encdct(QUEFRAG_CHAR.translate(BYTES_VAL, '&+=') + ' ')
-QUERY_VAL_ENCDCT = __allow_to_encdct(QUEFRAG_CHAR.translate(BYTES_VAL, '&+') + ' ')
+QUERY_KEY_ENCDCT = __allow_to_encdct(QUEFRAG_CHAR.translate(BYTES_VAL, "&+=") + " ")
+QUERY_VAL_ENCDCT = __allow_to_encdct(QUEFRAG_CHAR.translate(BYTES_VAL, "&+") + " ")
 
 # Host type alternatives:
 HOST_IP_LITERAL = 1
@@ -87,7 +86,7 @@ def __split_sz(s, n):
     return [s[b:b+n] for b in range(0, len(s), n)]
 
 def __valid_IPv4address(potential_ipv4):
-    if potential_ipv4[0] in "0123456789xX" and __all_in(potential_ipv4[1:], "0123456789.xX"):
+    if potential_ipv4[0] in (HEXDIG + "xX") and __all_in(potential_ipv4[1:], (HEXDIG + ".xX")):
         s_ipv4 = potential_ipv4.split('.', 4)
         if len(s_ipv4) == 4:
             try:
@@ -140,8 +139,8 @@ def __valid_leftIPv6(left_v6):
     return len(array_v6)
 
 def __valid_IPv6address(potential_ipv6):
-    sep_pos = potential_ipv6.find('::')
-    sep_count = potential_ipv6.count('::')
+    sep_pos = potential_ipv6.find("::")
+    sep_count = potential_ipv6.count("::")
     if sep_pos < 0:
         return __valid_rightIPv6(potential_ipv6) == 8
     elif sep_count == 1:
@@ -156,7 +155,7 @@ def __valid_IPv6address(potential_ipv6):
         return False
 
 def __valid_IPvFuture(potential_ipvf):
-    return bool(re.match(IPV_FUTURE_RE+'$', potential_ipvf))
+    return bool(re.match(IPV_FUTURE_RE + "$", potential_ipvf))
 
 def __valid_IPLiteral(potential_ipliteral):
     if len(potential_ipliteral) < 2 or potential_ipliteral[0] != '[' \
@@ -207,23 +206,18 @@ class InvalidQueryError(InvalidURIError):
 class InvalidFragmentError(InvalidURIError):
     """Invalid content for the fragment part of an URI"""
 
-PERCENT_CODE_SUB = re.compile('\%[\da-fA-F][\da-fA-F]').sub
+PERCENT_CODE_SUB = re.compile(r"\%[\da-fA-F][\da-fA-F]").sub
 
 def pct_decode(s):
     """
     Return the percent-decoded version of string s.
 
-    >>> def __split_sz(s, n):
-    ...     return [s[b:b+n] for b in range(0,len(s),n)]
-    ...
-    >>> s = ''.join(map(lambda x: '%'+x,
-    ...                 __split_sz(
-    ...                   binascii.hexlify("Coucou, je suis un satan"), 2))
-    ...             ).upper()
-    >>> print s
-    %43%6F%75%63%6F%75%2C%20%6A%65%20%73%75%69%73%20%75%6E%20%73%61%74%61%6E
-    >>> pct_decode(s)
-    'Coucou, je suis un satan'
+    >>> pct_decode('%43%6F%75%63%6F%75%2C%20%6A%65%20%73%75%69%73%20%63%6F%6E%76%69%76%69%61%6C')
+    'Coucou, je suis convivial'
+    >>> pct_decode('')
+    ''
+    >>> pct_decode('%2525')
+    '%25'
     """
     if s is None:
         return None
@@ -277,20 +271,20 @@ def host_type(host):
     representation of multiple bytes by a single decimal/octal/hexadecimal
     integer is not allowed.
 
-    Return HOST_IP_LITERAL, HOST_IPV4_ADDRESS or HOST_REG_NAME
+    Return 1 (HOST_IP_LITERAL), 2 (HOST_IPV4_ADDRESS) or 3 (HOST_REG_NAME)
 
     >>> host_type('[blablabla]')
-    HOST_IP_LITERAL
+    1
     >>> host_type('')
-    HOST_REG_NAME
+    3
     >>> host_type('127.0.0.1')
-    HOST_IPV4_ADDRESS
+    2
     >>> host_type('0x7F.0.0.00000000000001')
-    HOST_IPV4_ADDRESS
+    2
     >>> host_type('666.42.131.2')
-    HOST_REG_NAME
+    3
     >>> host_type('foobar.42')
-    HOST_REG_NAME
+    3
     """
     if not host:
         return HOST_REG_NAME
@@ -316,7 +310,7 @@ def split_authority(authority):
     Traceback (most recent call last):
       File "<stdin>", line 1, in ?
       File "<stdin>", line 26, in split_authority
-    InvalidIPLiteralError: Highly invalid IP-literal detected in URI authority "user@[host]:port"
+    InvalidIPLiteralError: Highly invalid IP-literal detected in URI authority 'user@[host]:port'
     >>> split_authority("user@[::dead:192.168.42.131]:port")
     ('user', None, '[::dead:192.168.42.131]', 'port')
     >>> split_authority("[::dead:192.168.42.131]:port")
@@ -343,21 +337,20 @@ def split_authority(authority):
         user, passwd, hostport = None, None, authority
     if hostport:
         if hostport[0] == '[':
-            m = re.match('\[([\da-fA-F:\.]+|'+IPV_FUTURE_RE
-                                 +')\](\:.*|)$', hostport)
+            m = re.match(r"\[([\da-fA-F:\.]+|" + IPV_FUTURE_RE
+                                 + r")\](\:.*|)$", hostport)
             if m:
                 host = '[' + m.group(1) + ']'
                 port = m.group(2)[1:]
             else:
-                raise InvalidIPLiteralError, 'Highly invalid IP-literal detected in URI authority "%s"' % authority
+                raise InvalidIPLiteralError, "Highly invalid IP-literal detected in URI authority %r" % (authority,)
         elif ':' in hostport:
             host, port = hostport.split(':', 1)
         else:
             host, port = hostport, None
     else:
         host, port = None, None
-    return (user and user or None, passwd and passwd or None,
-            host and host or None, port and port or None)
+    return (user or None, passwd or None, host or None, port or None)
 
 def split_query(query):
     """
@@ -367,8 +360,7 @@ def split_query(query):
     conversion.  Return:
 
     >>> split_query("k1=v1&k2=v+2%12&k3=&k4&&&k5==&=k&==")
-    (('k1', 'v1'), ('k2', 'v+2%12'), ('k3', ''), ('k4', None),
-            ('k5', '='), ('', 'k'), ('', '='))
+    (('k1', 'v1'), ('k2', 'v+2%12'), ('k3', ''), ('k4', None), ('k5', '='), ('', 'k'), ('', '='))
     """
     def split_assignment(a):
         sa = a.split('=', 1)
@@ -396,8 +388,8 @@ def basic_urisplit(uri):
     """
     Basic URI Parser according to RFC 3986
 
-    >>> urisplit("scheme://authority/path?query#fragment")
-    ('scheme', 'authority', 'path', 'query', 'fragment')
+    >>> basic_urisplit("scheme://authority/path?query#fragment")
+    ('scheme', 'authority', '/path', 'query', 'fragment')
     """
     p = RFC3986_MATCHER(uri).groups()
     return (p[1], p[3], p[4], p[6], p[8])
@@ -413,12 +405,7 @@ def uri_split_tree(uri):
     ...     'http://%42%20+blabla:lol@%77ww.foobar.org/%7Exilun/' +
     ...     '?query=+++%2b&=&===&a=b&&&+++aaa%3D=+%2B%2D&&&&' +
     ...     '#frag+++%42')
-    ('http',
-     ('%42%20+blabla', 'lol', '%77ww.foobar.org', None),
-     '/%7Exilun/',
-     (('query', '+++%2b'), ('', ''), ('', '=='),
-            ('a', 'b'), ('+++aaa%3D', '+%2B%2D')),
-     'frag+++%42')
+    ('http', ('%42%20+blabla', 'lol', '%77ww.foobar.org', None), '/%7Exilun/', (('query', '+++%2b'), ('', ''), ('', '=='), ('a', 'b'), ('+++aaa%3D', '+%2B%2D')), 'frag+++%42')
     """
     scheme, authority, path, query, fragment = basic_urisplit(uri)
     if authority:
@@ -433,7 +420,7 @@ def uri_tree_normalize(uri_tree):
     """
     Transform an URI tree so that adjacent all-empty fields are coalesced
     into a single None at parent level.
-    The return value can be used for validation by uri_tree_validate()
+    The return value can be used for validation.
     As a result, no distinction is made between empty and absent fields.
     It is believed that this limitation is harmless because this is the
     behavior of most implementations, and even useful in the context of
@@ -444,8 +431,8 @@ def uri_tree_normalize(uri_tree):
     If the distinction is ever really needed, for example to support empty
     anchor special HTTP script related URI in a clean way, one will
     probably need to completely rewrite (or at least review and modify)
-    this module, and special care would be taken to distinguish between '',
-    (), None and others everywhere implicit boolean conversion is now
+    this module, and special care would be needed to distinguish between '',
+    (), None, and others everywhere implicit boolean conversion is
     performed.  The behavior should then be checked in regards to its
     conformance with RFC 3986, especially (but this would probably not be
     sufficient) the classification switches of some URI parts according to
@@ -456,18 +443,15 @@ def uri_tree_normalize(uri_tree):
         authority = None
     if query:
         query = filter(lambda (x, y): bool(x) or bool(y), query)
-    return (scheme and scheme or None, authority and authority or None,
-            path and path or None, query and query or None,
-            fragment and fragment or None)
+    return (scheme or None, authority or None, path or None,
+            query or None, fragment or None)
 
 def uri_tree_validate(uri_tree):
     """
     Validate a tree splitted URI in format returned by
     uri_tree_normalize(), raising an exception in case something invalid
     is detected - that is RFC 3986 is not respected - and returning the
-    unmodified uri_tree otherwise, so this function can be used in an
-    helper function chaining uri_split_tree(), uri_tree_normalize(),
-    uri_tree_validate() and then uri_tree_decode().
+    unmodified uri_tree otherwise.
 
     This function must be called on something similar to the return value
     of uri_tree_normalize() - and not uri_tree_decode() or directly
@@ -494,35 +478,34 @@ def uri_tree_validate(uri_tree):
     scheme, authority, path, query, fragment = uri_tree
     if scheme:
         if not valid_scheme(scheme):
-            raise InvalidSchemeError, 'Invalid scheme "%s"' % scheme
+            raise InvalidSchemeError, "Invalid scheme %r" % (scheme,)
     if authority:
         user, passwd, host, port = authority
         if user and not __all_in(user, USER_CHAR):
-            raise InvalidUserError, 'Invalid user "%s"' % user
+            raise InvalidUserError, "Invalid user %r" % (user,)
         if passwd and not __all_in(passwd, PASSWD_CHAR):
-            raise InvalidPasswdError, 'Invalid passwd "%s"' % passwd
+            raise InvalidPasswdError, "Invalid passwd %r" % (passwd,)
         if host:
             type_host = host_type(host)
             if type_host == HOST_REG_NAME:
                 if not __all_in(host, REG_NAME_CHAR):
-                    raise InvalidRegNameError, 'Invalid reg-name "%s"' % host
+                    raise InvalidRegNameError, "Invalid reg-name %r" % (host,)
             elif type_host == HOST_IP_LITERAL:
                 if not __valid_IPLiteral(host):
-                    raise InvalidIPLiteralError, 'Invalid IP-literal "%s"' % host
+                    raise InvalidIPLiteralError, "Invalid IP-literal %r" % (host,)
         if port and not __all_in(port, DIGIT):
-            raise InvalidPortError, 'Invalid port "%s"' % port
+            raise InvalidPortError, "Invalid port %r" % (port,)
     if path:
         if not __all_in(path, PCHAR):
-            raise InvalidPathError, 'Invalid path "%s" - invalid character detected' % path
+            raise InvalidPathError, "Invalid path %r - invalid character detected" % (path,)
         if authority and path[0] != '/':
-            raise InvalidPathError, 'Invalid path "%s" - non-absolute path can\'t be used with an authority' % path
-        if (not authority) and (not scheme) \
-           and (':' in path.split('/', 1)[0]):
-            raise InvalidPathError, 'Invalid path "%s" - path-noscheme can\'t have a \':\' if no \'/\' before' % path
+            raise InvalidPathError, "Invalid path %r - non-absolute path can't be used with an authority" % (path,)
+        if (not authority) and (not scheme) and (':' in path.split('/', 1)[0]):
+            raise InvalidPathError, "Invalid path %r - path-noscheme can't have a ':' if no '/' before" % (path,)
     if query and (not __valid_query(query)):
-        raise InvalidQueryError, 'Invalid splitted query tuple "%s"' % str(query)
+        raise InvalidQueryError, "Invalid splitted query tuple %r" % (query,)
     if fragment and (not __all_in(fragment, QUEFRAG_CHAR)):
-        raise InvalidFragmentError, 'Invalid fragment "%s"' % fragment
+        raise InvalidFragmentError, "Invalid fragment %r" % (fragment,)
     return uri_tree
 
 def uri_tree_decode(uri_tree):
@@ -538,12 +521,7 @@ def uri_tree_decode(uri_tree):
     ...             'http://%42%20+blabla:lol@%77ww.foobar.org/%7Exilun/' +
     ...             '?query=+++%2b&=&===&a=b&&&+++aaa%3D=+%2B%2D&&&&' +
     ...             '#frag+++%42'))
-    ('http',
-     ('B +blabla', 'lol', 'www.foobar.org', None),
-     '/~xilun/',
-     (('query', '   +'), ('', ''), ('', '=='),
-            ('a', 'b'), ('   aaa=', ' +-')),
-     'frag+++B')
+    ('http', ('B +blabla', 'lol', 'www.foobar.org', None), '/~xilun/', (('query', '   +'), ('', ''), ('', '=='), ('a', 'b'), ('   aaa=', ' +-')), 'frag+++B')
     """
     scheme, authority, path, query, fragment = uri_tree
     if authority:
@@ -557,7 +535,7 @@ def uri_tree_decode(uri_tree):
         query = tuple([(query_elt_decode(x), query_elt_decode(y)) for (x, y) in query])
     return (scheme, authority, path, query, fragment)
 
-def uri_split_norm_valid_decode(uri):
+def uri_help_split(uri):
     """
     Return uri_tree_decode(
                     uri_tree_validate(
@@ -568,7 +546,6 @@ def uri_split_norm_valid_decode(uri):
                     uri_tree_validate(
                             uri_tree_normalize(
                                     uri_split_tree(uri))))
-uri_help_split = uri_split_norm_valid_decode
 
 def uri_tree_precode_check(uri_tree, type_host = HOST_REG_NAME):
     """
@@ -578,17 +555,20 @@ def uri_tree_precode_check(uri_tree, type_host = HOST_REG_NAME):
     scheme, authority, path, query, fragment = uri_tree # pylint: disable-msg=W0612
     if scheme:
         if not valid_scheme(scheme):
-            raise InvalidSchemeError, 'Invalid scheme "%s"' % scheme
+            raise InvalidSchemeError, "Invalid scheme %r" % (scheme,)
     if authority:
         user, passwd, host, port = authority # pylint: disable-msg=W0612
         if port and not __all_in(port, DIGIT):
-            raise InvalidPortError, 'Invalid port "%s"' % port
+            raise InvalidPortError, "Invalid port %r" % (port,)
         if type_host == HOST_IP_LITERAL:
             if host and (not __valid_IPLiteral(host)):
-                raise InvalidIPLiteralError, 'Invalid IP-literal "%s"' % host
+                raise InvalidIPLiteralError, "Invalid IP-literal %r" % (host,)
         elif type_host == HOST_IPV4_ADDRESS:
             if host and (not __valid_IPv4address(host)):
-                raise InvalidIPv4addressError, 'Invalid IPv4address "%s"' % host
+                raise InvalidIPv4addressError, "Invalid IPv4address %r" % (host,)
+    if path:
+        if authority and path[0] != '/':
+            raise InvalidPathError, "Invalid path %r - non-absolute path can't be used with an authority" % (path,)
     return uri_tree
 
 def uri_tree_encode(uri_tree, type_host = HOST_REG_NAME):
@@ -607,10 +587,7 @@ def uri_tree_encode(uri_tree, type_host = HOST_REG_NAME):
         authority = (user, passwd, host, port)
     if path:
         path = pct_encode(path, P_ENCDCT)
-        if authority:
-            if path[0] != '/':
-                path = '/' + path
-        elif (not scheme):
+        if (not authority) and (not scheme):
             # check for path-noscheme special case
             sppath = path.split('/', 1)
             if ':' in sppath[0]:
@@ -672,22 +649,27 @@ def uri_unsplit_tree(uri_tree):
         uri += '#' + fragment
     return uri
 
-def uri_tree_prechk_code_norm_unsplit(uri_tree):
+def uri_help_unsplit(uri_tree):
     """
     Return uri_unsplit_tree(
-                   uri_tree_normalize(
-                           uri_tree_encode(
-                                   uri_tree_precode_check(uri_tree))))
+                   uri_tree_encode(
+                           uri_tree_precode_check(
+                                   uri_tree_normalize(uri_tree))))
     """
     return uri_unsplit_tree(
-                   uri_tree_normalize(
-                           uri_tree_encode(
-                                   uri_tree_precode_check(uri_tree))))
-uri_help_unsplit = uri_tree_prechk_code_norm_unsplit
+                   uri_tree_encode(
+                           uri_tree_precode_check(
+                                   uri_tree_normalize(uri_tree))))
 
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == "__main__":
+    _test()
 
 #
-# TODO: write non regression tests
+# TODO: write more tests
 #
 # ex: ('http', ('xilun:/?#[]@!$&\'"()*+,;=-._~%:@/?% ', 'kikoolol/?#[]@!$&\'"()*+,;=-._~%:@/?% ', 'www./?#[]@!$&\'"()*+,;=-._~%:@/? %xivo.fr', '8080'), '/kikoololerie//?#[]@!$&\'"()*+,;=-. _~%:@/?%', (('k1/?#[]@!$&\'"()*+,;=-._~%:@/?% ', 'v1/?#[]@!$&\'"()*+,;=-._~%:@/?% '), ('k2/?#[]@!$&\'"()*+,;=-._~%:@/?% ', 'v2/?#[]@!$&\'"()*+,;=-._~%:@/?% ')), 'foobar2000/?#[]@!$&\'"()*+,;=-._~%:@/?% ')
 #     -> "http://xilun%3A%2F%3F%23%5B%5D%40!$&'%22()*+,;=-._~%25%3A%40%2F%3F%25%20:kikoolol%2F%3F%23%5B%5D%40!$&'%22()*+,;=-._~%25:%40%2F%3F%25%20@www.%2F%3F%23%5B%5D%40!$&'%22()*+,;=-._~%25%3A%40%2F%3F%20%25xivo.fr:8080/kikoololerie//%3F%23%5B%5D@!$&'%22()*+,;=-.%20_~%25:@/%3F%25?k1/?%23%5B%5D@!$%26'%22()*%2B,;%3D-._~%25:@/?%25+=v1/?%23%5B%5D@!$%26'%22()*%2B,;=-._~%25:@/?%25+&k2/?%23%5B%5D@!$%26'%22()*%2B,;%3D-._~%25:@/?%25+=v2/?%23%5B%5D@!$%26'%22()*%2B,;=-._~%25:@/?%25+#foobar2000/?%23%5B%5D@!$&'%22()*+,;=-._~%25:@/?%25%20"
