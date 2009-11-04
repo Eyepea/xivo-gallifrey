087b796138f9b93a931271afcf8ff502d30d6925
Adrien DELLE CAVE (Decryptus) <decryptus@proformatique.com>
2009-11-04 18:44:45 +0000

    [bugfix]
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@7046 dc85baef-7018-0410-bfbd-8a639da648bb

--- agid/xivo_agid/modules/user_set_call_rights.py
+++ agid/xivo_agid/modules/user_set_call_rights.py
@@ -44,49 +44,52 @@ def _user_set_call_rights(agi, cursor, args):
     try:
         user = objects.User(agi, cursor, int(userid))
     except (ValueError, LookupError):
-        call_rights.allow(agi)
-
-    cursor.query("SELECT ${columns} FROM rightcall "
-                 "INNER JOIN rightcallmember "
-                 "ON rightcall.id = rightcallmember.rightcallid "
-                 "WHERE rightcall.id IN " + rightcallids + " "
-                 "AND rightcallmember.type = 'user' "
-                 "AND rightcallmember.typeval = %s "
-                 "AND rightcall.commented = 0",
-                 (call_rights.RIGHTCALL_AUTHORIZATION_COLNAME, call_rights.RIGHTCALL_PASSWD_COLNAME),
-                 (user.id,))
-    res = cursor.fetchall()
-    call_rights.apply_rules(agi, res)
-
-    cursor.query("SELECT ${columns} FROM groupfeatures "
-                 "INNER JOIN queuemember "
-                 "ON groupfeatures.name = queuemember.queue_name "
-                 "INNER JOIN queue "
-                 "ON queue.name = queuemember.queue_name "
-                 "WHERE groupfeatures.deleted = 0 "
-                 "AND queuemember.userid = %s "
-                 "AND queuemember.usertype = 'user' "
-                 "AND queuemember.category = 'group' "
-                 "AND queuemember.commented = 0 "
-                 "AND queue.category = 'group' "
-                 "AND queue.commented = 0",
-                 ('groupfeatures.id',),
-                 (user.id,))
-    res = cursor.fetchall()
-
-    if res:
-        groupids = [row['groupfeatures.id'] for row in res]
+        if not outcallid:
+            call_rights.allow(agi)
+        else:
+            pass
+    else:
         cursor.query("SELECT ${columns} FROM rightcall "
                      "INNER JOIN rightcallmember "
                      "ON rightcall.id = rightcallmember.rightcallid "
                      "WHERE rightcall.id IN " + rightcallids + " "
-                     "AND rightcallmember.type = 'group' "
-                     "AND rightcallmember.typeval IN (" + ", ".join(["%s"] * len(res)) + ") "
+                     "AND rightcallmember.type = 'user' "
+                     "AND rightcallmember.typeval = %s "
                      "AND rightcall.commented = 0",
                      (call_rights.RIGHTCALL_AUTHORIZATION_COLNAME, call_rights.RIGHTCALL_PASSWD_COLNAME),
-                     groupids)
+                     (user.id,))
         res = cursor.fetchall()
         call_rights.apply_rules(agi, res)
+
+        cursor.query("SELECT ${columns} FROM groupfeatures "
+                     "INNER JOIN queuemember "
+                     "ON groupfeatures.name = queuemember.queue_name "
+                     "INNER JOIN queue "
+                     "ON queue.name = queuemember.queue_name "
+                     "WHERE groupfeatures.deleted = 0 "
+                     "AND queuemember.userid = %s "
+                     "AND queuemember.usertype = 'user' "
+                     "AND queuemember.category = 'group' "
+                     "AND queuemember.commented = 0 "
+                     "AND queue.category = 'group' "
+                     "AND queue.commented = 0",
+                     ('groupfeatures.id',),
+                     (user.id,))
+        res = cursor.fetchall()
+
+        if res:
+            groupids = [row['groupfeatures.id'] for row in res]
+            cursor.query("SELECT ${columns} FROM rightcall "
+                         "INNER JOIN rightcallmember "
+                         "ON rightcall.id = rightcallmember.rightcallid "
+                         "WHERE rightcall.id IN " + rightcallids + " "
+                         "AND rightcallmember.type = 'group' "
+                         "AND rightcallmember.typeval IN (" + ", ".join(["%s"] * len(res)) + ") "
+                         "AND rightcall.commented = 0",
+                         (call_rights.RIGHTCALL_AUTHORIZATION_COLNAME, call_rights.RIGHTCALL_PASSWD_COLNAME),
+                         groupids)
+            res = cursor.fetchall()
+            call_rights.apply_rules(agi, res)
 
     if outcallid:
         cursor.query("SELECT ${columns} FROM rightcall "
