da2710f35bcdadf59edb55e0802962e53234859a
Adrien DELLE CAVE (Decryptus) <decryptus@proformatique.com>
2010-01-10 15:45:16 +0000

    [clean]
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@7406 dc85baef-7018-0410-bfbd-8a639da648bb

--- web-interface/tpl/www/struct/page/simple.php
+++ web-interface/tpl/www/struct/page/simple.php
@@ -18,8 +18,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-	$menu = &$this->get_module('menu');
-	$this->file_include('bloc/head');
+$menu = &$this->get_module('menu');
+$this->file_include('bloc/head');
+
 ?>
 <div id="bc-body">
 <div id="bpc-simple">
