00c4087ae8557e11d2ae66e47c71ea969109ee2d
Cédric Abunar <cabunar@proformatique.com>
2010-04-01 15:53:29 +0000

    update page contact; add avencall, pf ca.
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@7857 dc85baef-7018-0410-bfbd-8a639da648bb

--- web-interface/tpl/www/bloc/xivo/contact.php
+++ web-interface/tpl/www/bloc/xivo/contact.php
@@ -23,24 +23,60 @@
 <div class="b-infos">
 	<h3 class="sb-top xspan"><span class="span-left">&nbsp;</span><span class="span-center"><?=$this->bbf('title_content_name');?></span><span class="span-right">&nbsp;</span></h3>
 	<div class="sb-content">
-	<div>
-		<?=$url->img_html('img/site/proformatique.png',XIVO_CORP_LABEL);?>
+		<div class="logo"><?=$url->img_html('img/site/avencall.png',XIVO_AV_FR_CORP_NAME);?></div>
+		<dl class="body">
+			<dt><?=$this->bbf('info_address');?></dt>
+				<dd><?=XIVO_AV_FR_CORP_ADDRESS?></dd>
+				<dd><?=XIVO_AV_FR_CORP_ZIPCODE?> <?=XIVO_AV_FR_CORP_CITY?></dd>
+				<dd><?=XIVO_AV_FR_CORP_COUNTRY?></dd>
+			<dt><?=$this->bbf('info_phone');?></dt>
+				<dd><?=XIVO_AV_FR_CORP_PHONE?></dd>
+			<dt><?=$this->bbf('info_fax');?></dt>
+				<dd><?=XIVO_AV_FR_CORP_FAX?></dd>
+			<dt><?=$this->bbf('info_e-mail');?></dt>
+				<dd><?='<a href="mailto:',XIVO_AV_FR_CORP_EMAIL,'" title="',XIVO_AV_FR_CORP_EMAIL,'">',XIVO_AV_FR_CORP_EMAIL,'</a>'?></dd>
+			<dt><?=$this->bbf('info_websites');?></dt>
+				<dd><?='<a href="http://',XIVO_AV_FR_CORP_URL,'" title="',XIVO_AV_FR_CORP_URL,'" target="_blank">',XIVO_AV_FR_CORP_URL,'</a>'?></dd>
+		</dl>
+		<div class="clear"></div>
 	</div>
-<dl>
-	<dt><?=$this->bbf('info_address');?></dt>
-	<dd><?=XIVO_CORP_ADDRESS?></dd>
-	<dd><?=XIVO_CORP_ZIPCODE?> <?=XIVO_CORP_CITY?></dd>
-	<dd><?=XIVO_CORP_COUNTRY?></dd>
-	<dt><?=$this->bbf('info_phone');?></dt>
-	<dd><?=XIVO_CORP_PHONE?></dd>
-	<dt><?=$this->bbf('info_fax');?></dt>
-	<dd><?=XIVO_CORP_FAX?></dd>
-	<dt><?=$this->bbf('info_e-mail');?></dt>
-	<dd><?='<a href="mailto:',XIVO_CORP_EMAIL,'" title="',XIVO_CORP_EMAIL,'">',XIVO_CORP_EMAIL,'</a>'?></dd>
-	<dt><?=$this->bbf('info_websites');?></dt>
-	<dd><?='<a href="http://',XIVO_CORP_URL,'" title="',XIVO_CORP_LABEL,'" target="_blank">',XIVO_CORP_URL,'</a>'?></dd>
-	<dd><?='<a href="http://',XIVO_SOFT_URL,'" title="',XIVO_SOFT_LABEL,'" target="_blank">',XIVO_SOFT_URL,'</a>'?></dd>
-</dl>
+	<div class="sb-content">
+		<div class="logo"><?=$url->img_html('img/site/proformatique-fr.png',XIVO_PF_FR_CORP_NAME);?></div>
+		<dl class="body">
+			<dt><?=$this->bbf('info_address');?></dt>
+				<dd><?=XIVO_PF_FR_CORP_ADDRESS?></dd>
+				<dd><?=XIVO_PF_FR_CORP_ZIPCODE?> <?=XIVO_PF_FR_CORP_CITY?></dd>
+				<dd><?=XIVO_PF_FR_CORP_COUNTRY?></dd>
+			<dt><?=$this->bbf('info_phone');?></dt>
+				<dd><?=XIVO_PF_FR_CORP_PHONE?></dd>
+			<dt><?=$this->bbf('info_fax');?></dt>
+				<dd><?=XIVO_PF_FR_CORP_FAX?></dd>
+			<dt><?=$this->bbf('info_e-mail');?></dt>
+				<dd><?='<a href="mailto:',XIVO_PF_FR_CORP_EMAIL,'" title="',XIVO_PF_FR_CORP_EMAIL,'">',XIVO_PF_FR_CORP_EMAIL,'</a>'?></dd>
+			<dt><?=$this->bbf('info_websites');?></dt>
+				<dd><?='<a href="http://',XIVO_PF_FR_CORP_URL,'" title="',XIVO_PF_FR_CORP_LABEL,'" target="_blank">',XIVO_PF_FR_CORP_URL,'</a>'?></dd>
+				<dd><?='<a href="http://',XIVO_SOFT_URL,'" title="',XIVO_SOFT_LABEL,'" target="_blank">',XIVO_SOFT_URL,'</a>'?></dd>
+		</dl>
+		<div class="clear"></div>
+	</div>
+	<div class="sb-content">
+		<div class="logo"><?=$url->img_html('img/site/proformatique-ca.png',XIVO_PF_FR_CORP_NAME);?></div>
+		<dl class="body">
+			<dt><?=$this->bbf('info_address');?></dt>
+				<dd><?=XIVO_PF_CA_CORP_ADDRESS?></dd>
+				<dd><?=XIVO_PF_CA_CORP_ZIPCODE?> <?=XIVO_PF_CA_CORP_CITY?></dd>
+				<dd><?=XIVO_PF_CA_CORP_COUNTRY?></dd>
+			<dt><?=$this->bbf('info_phone');?></dt>
+				<dd><?=XIVO_PF_CA_CORP_PHONE?></dd>
+			<dt><?=$this->bbf('info_fax');?></dt>
+				<dd><?=XIVO_PF_CA_CORP_FAX?></dd>
+			<dt><?=$this->bbf('info_e-mail');?></dt>
+				<dd><?='<a href="mailto:',XIVO_PF_CA_CORP_EMAIL,'" title="',XIVO_PF_CA_CORP_EMAIL,'">',XIVO_PF_CA_CORP_EMAIL,'</a>'?></dd>
+			<dt><?=$this->bbf('info_websites');?></dt>
+				<dd><?='<a href="http://',XIVO_PF_CA_CORP_URL,'" title="',XIVO_PF_CA_CORP_URL,'" target="_blank">',XIVO_PF_CA_CORP_URL,'</a>'?></dd>
+				<dd><?='<a href="http://',XIVO_SOFT_URL,'" title="',XIVO_SOFT_LABEL,'" target="_blank">',XIVO_SOFT_URL,'</a>'?></dd>
+		</dl>
+		<div class="clear"></div>
 	</div>
 	<div class="sb-foot xspan"><span class="span-left">&nbsp;</span><span class="span-center">&nbsp;</span><span class="span-right">&nbsp;</span></div>
 </div>
