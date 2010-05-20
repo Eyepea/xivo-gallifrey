1e7034ef3525b3c2395385e53f3142fdcc11cde1
Corentin Le Gall <clegall@proformatique.com>
2010-05-20 15:35:27 +0000

    [build] define a daemon-like name and path (source part)
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@8166 dc85baef-7018-0410-bfbd-8a639da648bb

--- queues-logger/setup.py
+++ queues-logger/setup.py
@@ -7,14 +7,15 @@ from distutils.core import setup
 setup(
       name='queue_logger',
       version='1.0',
-      description='XIVO queue logger',
+      description='XiVO queue logger',
       author='Proformatique',
       author_email='technique@proformatique.com',
       url='http://xivo.fr/',
       packages=['xivo_queuelogger'],
-      scripts=['queuelogger.py'],
+      scripts=['queuelogger'],
       license="GNU General Public License 3",
       data_files=[
+          ('/usr/sbin', ['queuelogger']),
           ('/usr/share/pf-xivo-queues-logger/sqlite3/', ['db/create.sql'])
       ]
      )
