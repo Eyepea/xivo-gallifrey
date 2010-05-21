64b9aeda5642be06288bc9467db4fb380f961bcf
Guillaume Bour <gbour@proformatique.com>
2010-05-21 15:11:55 +0000

    [fix] user add/edit not display because php 5.2 does not support inline functions
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@8182 dc85baef-7018-0410-bfbd-8a639da648bb

--- web-interface/action/www/service/ipbx/asterisk/pbx_settings/users/add.php
+++ web-interface/action/www/service/ipbx/asterisk/pbx_settings/users/add.php
@@ -90,11 +90,12 @@ if(isset($_QR['fm_send']) === true
 #		// sort values
 		unset($positions[count($positions)-1]);
 		$idxs = array_keys($positions);
-		$res = usort(&$idxs, 
-			function($x, $y) {
+
+		function pos_sort($x, $y) {
 				global $positions;
 				return $positions[$x] - $positions[$y];
-			});
+		}
+		$res = usort(&$idxs, "pos_sort");
 
 		foreach($idxs as $idx)
 			$cursoftkey[] = $values[$idx];
