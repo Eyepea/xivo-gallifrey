a90326d693fc687f3861ac18dbe9c2c6a4a99f67
Corentin Le Gall <clegall@proformatique.com>
2010-06-11 09:57:29 +0000

    [bugfix] right path to add a context's directory options
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@8332 dc85baef-7018-0410-bfbd-8a639da648bb

--- web-interface/tpl/www/bloc/menu/toolbar/cti/contexts.php
+++ web-interface/tpl/www/bloc/menu/toolbar/cti/contexts.php
@@ -36,7 +36,7 @@ echo	$url->href_html($url->img_html('img/menu/top/toolbar/bt-add.gif',
 				       $this->bbf('toolbar_opt_add'),
 				       'id="toolbar-bt-add"
 					border="0"'),
-			'service/ipbx/cti_settings/contexts',
+			'cti/index.php/contexts/',
 			'act=add',
 			null,
 			$this->bbf('toolbar_opt_add'));
