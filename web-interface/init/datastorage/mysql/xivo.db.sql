9893865cc1b0e9122acb6bcf38162e0de4ef959b
Cédric Abunar <cabunar@proformatique.com>
2010-06-19 07:56:53 +0000

    [evol] add queuelogger table in mysql xivo database
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@8420 dc85baef-7018-0410-bfbd-8a639da648bb

--- web-interface/init/datastorage/mysql/xivo.db.sql
+++ web-interface/init/datastorage/mysql/xivo.db.sql
@@ -296,6 +296,24 @@ CREATE TABLE `monitoring` (
 
 INSERT INTO monitoring VALUES (1,0,NULL,NULL,NULL);
 
+
+DROP TABLE IF EXISTS `queue_info`;
+CREATE TABLE `queue_info` (
+ `id` int(10) unsigned auto_increment,
+ `call_time_t` int(10) unsigned,
+ `queue_name` varchar(255) NOT NULL,
+ `caller` varchar(255) NOT NULL,
+ `caller_uniqueid` varchar(255) NOT NULL,
+ `call_picker` varchar(255) NOT NULL,
+ `hold_time` int,
+ `talk_time` int,
+ PRIMARY KEY(`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE INDEX `queue_info_call_time_t_index` ON `queue_info`(`call_time_t`);
+CREATE INDEX `queue_info_queue_name_index` ON `queue_info`(`queue_name`);
+
+
 -- HA
 DROP TABLE IF EXISTS `ha`;
 CREATE TABLE `ha` (
