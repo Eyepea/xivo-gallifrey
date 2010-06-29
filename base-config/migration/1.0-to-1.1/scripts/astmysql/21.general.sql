4d3af999de15a27b4aff889f079b2e412a5a09cb
Guillaume Bour <gbour@proformatique.com>
2010-06-29 08:28:19 +0000

    [evol] ticket #975: update migration scripts
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@8479 dc85baef-7018-0410-bfbd-8a639da648bb

---
+++ base-config/migration/1.0-to-1.1/scripts/astmysql/21.general.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE `general`
+(
+ `id`       int(10) unsigned auto_increment,
+ `timezone` varchar(128),
+ PRIMARY KEY(`id`)
+);
+
+INSERT INTO `general` VALUES (1, 'Europe/Paris');
+
---
+++ base-config/migration/1.0-to-1.1/scripts/astsqlite/21.general.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE general
+(
+ id       integer unsigned,
+ timezone varchar(128),
+ PRIMARY KEY(`id`)
+);
+
+INSERT INTO general VALUES (1, 'Europe/Paris');
+
