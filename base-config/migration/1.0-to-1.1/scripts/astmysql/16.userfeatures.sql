d27107617f419e0dbcad37f203dfe6fa46bf298d
Guillaume Bour <gbour@proformatique.com>
2010-06-29 13:41:17 +0000

    [evol] ticket #975: update migration scripts
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@8501 dc85baef-7018-0410-bfbd-8a639da648bb

--- base-config/migration/1.0-to-1.1/scripts/astmysql/16.userfeatures.sql
+++ base-config/migration/1.0-to-1.1/scripts/astmysql/16.userfeatures.sql
@@ -1,4 +1,5 @@
 
 
 ALTER TABLE `userfeatures` CHANGE COLUMN `callfilter` `incallfilter` tinyint(1) NOT NULL DEFAULT 0;
+ALTER TABLE `userfeatures` ADD COLUMN `timezone` varchar(128) AFTER `internal`;
 
