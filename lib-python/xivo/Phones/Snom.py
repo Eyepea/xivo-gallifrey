69ef3feaa62056b9920db570d7e83f52ca7635dd
Sylvain Boily <sboily@proformatique.com>
2010-07-29 21:38:42 +0000

    [fix] dest to blf
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/branches/official/1.1-gallifrey@8794 dc85baef-7018-0410-bfbd-8a639da648bb

--- lib-python/xivo/Phones/Snom.py
+++ lib-python/xivo/Phones/Snom.py
@@ -109,7 +109,7 @@ class Snom(PhoneVendorMixin):
             else:
                 xtype = "speed"
 
-            fk_config_lines.append("<fkey idx=\"%01d\" context=\"active\" perm=\"\">%s <sip:%s@%s></fkey>" % (int(key)-1, xtype, exten, cls.ASTERISK_IPV4))
+            fk_config_lines.append("<fkey idx=\"%01d\" context=\"active\" perm=\"\">%s &lt;sip:%s@%s&gt;</fkey>" % (int(key)-1, xtype, exten, cls.ASTERISK_IPV4))
         return "\n".join(fk_config_lines)
 
     def do_reinit(self):
