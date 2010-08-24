ae7fc2e8338394380bd35d2ae5846fc12bef8451
Etienne Desrochers-Lessard <hexanol@proformatique.com>
2010-08-24 14:10:59 +0000

    [evol/fix] gallifrey branch / provisioning of the Gigaset C470IP is now a 1 step process (instead of 2).
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/branches/official/1.1-gallifrey@9001 dc85baef-7018-0410-bfbd-8a639da648bb

--- lib-python/xivo/Phones/Siemens.py
+++ lib-python/xivo/Phones/Siemens.py
@@ -586,9 +586,15 @@ class Siemens(PhoneVendorMixin):
 
     def __provi(self):
         "Provisioning the phone."
-        if self.phone.get('ipv4'):
-            http = SiemensHTTP(self.SIEMENS_COMMON_DIR, self.SIEMENS_COMMON_PIN)
-            http.provi(self.phone['ipv4'], self.phone['model'], self.phone['macaddr'])
+        ipv4 = self.phone.get('ipv4')
+        if not ipv4:
+            ipv4 = xivo_config.ipv4_from_macaddr(self.phone["macaddr"])
+            if not ipv4:
+                log.warning("Won't be able to provision the phone %s - no IP address found" % self.phone['model'])
+                return
+        assert ipv4
+        http = SiemensHTTP(self.SIEMENS_COMMON_DIR, self.SIEMENS_COMMON_PIN)
+        http.provi(ipv4, self.phone['model'], self.phone['macaddr'])
 
     def __action_prov(self, provinfo):
         if provinfo['sha1sum'] == '0':
