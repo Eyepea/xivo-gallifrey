a7130dcb3a77158e3020040133f66b0991aa5a10
Guillaume Bour <gbour@proformatique.com>
2010-08-25 08:52:54 +0000

    [fix] remove extra CR from migration script
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/branches/official/1.1-gallifrey@9010 dc85baef-7018-0410-bfbd-8a639da648bb

--- base-config/migration/1.1-to-1.1.3/mysql/02.ctisheetactions.sql
+++ base-config/migration/1.1-to-1.1.3/mysql/02.ctisheetactions.sql
@@ -22,7 +22,6 @@ INSERT INTO `ctisheetactions` VALUES(7,'queue','sheet_action_queue','["default"]
 INSERT INTO `ctisheetactions` VALUES(8,'custom1','sheet_action_custom1','["default"]','all','["agentsup","agent","client"]','{"10": [ "","text","Inconnu","Appel {xivo-direction} (Custom)" ],"20": [ "Numéro entrant","phone","Inconnu","{xivo-calleridnum}" ],"30": [ "Nom","text","Inconnu","{db-fullname}" ]}','{"10": [ "","title","","Appel {xivo-direction} (Custom)" ],"20": [ "","body","Inconnu","appel de {xivo-calleridnum} pour {xivo-calledidnum}" ],"30": [ "","body","Inconnu","{db-fullname} (selon {xivo-directory})" ],"40": [ "","body","","le {xivo-date}, il est {xivo-time}" ]}','','{}','',1)
 	ON DUPLICATE KEY UPDATE
 		`sheet_info`   = '{"10": [ "","text","Inconnu","Appel {xivo-direction} (Custom)" ],"20": [ "Nummro entrant","phone","Inconnu","{xivo-calleridnum}" ],"30": [ "Nom","text","Inconnu","{db-fullname}" ]}',
-		`systray_info` = '{"10": [ "","title","","Appel {xivo-direction} (Custom)" ],"20": [ "","body","Inconnu","appel dd
-e {xivo-calleridnum} pour {xivo-calledidnum}" ],"30": [ "","body","Inconnu","{db-fullname} (selon {xivo-directory})" ],"40": [ "","body","","le {xivo-date}, il est {xivo-time}" ]}',
+		`systray_info` = '{"10": [ "","title","","Appel {xivo-direction} (Custom)" ],"20": [ "","body","Inconnu","appel de {xivo-calleridnum} pour {xivo-calledidnum}" ],"30": [ "","body","Inconnu","{db-fullname} (selon {xivo-directory})" ],"40": [ "","body","","le {xivo-date}, il est {xivo-time}" ]}',
 		`sheet_qtui`   = '',
 		`action_info`  = '{}';
