20c2c3af54a498377cccccb75e821bc5d11ee834
Guillaume Bour <gbour@proformatique.com>
2010-09-09 14:57:14 +0000

    [fix] add vlan_id field in migration scripts
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/branches/official/1.1-gallifrey@9141 dc85baef-7018-0410-bfbd-8a639da648bb

--- web-interface/migration/1.1-to-1.1.4/mysql/01.provisioning.sql
+++ web-interface/migration/1.1-to-1.1.4/mysql/01.provisioning.sql
@@ -6,11 +6,12 @@ CREATE TABLE `provisioning` (
  `registrar_backup` varchar(255) NOT NULL DEFAULT '',
  `proxy_main`       varchar(255) NOT NULL DEFAULT '',
  `proxy_backup`     varchar(255) NOT NULL DEFAULT '',
+ `vlan_id`          integer,
  PRIMARY KEY(`id`)
 ) ENGINE=MyISAM;
 
 -- we set default registrar_main and proxy_main value as VOIP interface address
 -- if such interface does not exist, we set empty values
-INSERT IGNORE INTO `provisioning` SELECT 1, `address`, '', `address`, '' FROM `netiface` WHERE `networktype` = 'voip' LIMIT 1;
-INSERT IGNORE INTO `provisioning` VALUES(1, '', '', '', '');
+INSERT IGNORE INTO `provisioning` SELECT 1, `address`, '', `address`, '', `vlanid` FROM `netiface` WHERE `networktype` = 'voip' LIMIT 1;
+INSERT IGNORE INTO `provisioning` VALUES(1, '', '', '', '', NULL);
 
