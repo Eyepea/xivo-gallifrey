784b47ed5f34213b8e03f53c02ec9fcab18e6d33
Guillaume Bour <gbour@proformatique.com>
2011-01-31 15:20:17 +0000

    [fix] ticket #1680: migration scripts
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/branches/official/1.1-gallifrey@10084 dc85baef-7018-0410-bfbd-8a639da648bb

---
+++ base-config/migration/1.1.11-to-1.1.12/mysql/01.features.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `features` VALUES (NULL,0,0,0,'features.conf','general','parkedcalltransfers','both');
+INSERT INTO `features` VALUES (NULL,0,0,0,'features.conf','general','parkedcallreparking','no');
+INSERT INTO `features` VALUES (NULL,0,0,0,'features.conf','general','parkedcallhangup','no');
+INSERT INTO `features` VALUES (NULL,0,0,0,'features.conf','general','parkedcallrecording','no');
+INSERT INTO `features` VALUES (NULL,0,0,0,'features.conf','general','atxfernoanswertimeout',30);
