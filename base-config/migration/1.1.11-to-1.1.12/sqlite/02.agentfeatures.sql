8c6f94412439ea48dfc6a8c7261dbe908bb2f6aa
Guillaume Bour <gbour@proformatique.com>
2011-02-01 09:27:02 +0000

    [fix] ticket #1843: update voicemail/agent en language (to en_US) when migrating
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/branches/official/1.1-gallifrey@10087 dc85baef-7018-0410-bfbd-8a639da648bb

--- base-config/migration/1.1.11-to-1.1.12/sqlite/02.agentfeatures.sql
+++ base-config/migration/1.1.11-to-1.1.12/sqlite/02.agentfeatures.sql
@@ -1,3 +1,4 @@
 
 UPDATE agentfeatures SET language = 'fr_FR' WHERE language = 'fr';
+UPDATE agentfeatures SET language = 'en_US' WHERE language = 'en';
 
--- base-config/migration/1.1.11-to-1.1.12/sqlite/03.voicemail.sql
+++ base-config/migration/1.1.11-to-1.1.12/sqlite/03.voicemail.sql
@@ -1,2 +1,3 @@
 
 UPDATE voicemail SET language = 'fr_FR' WHERE language = 'fr';
+UPDATE voicemail SET language = 'en_US' WHERE language = 'en';
